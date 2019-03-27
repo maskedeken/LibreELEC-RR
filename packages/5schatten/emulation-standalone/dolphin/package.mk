fbefdb64b057f202b1d743fcc6b516974b51fa09
5schatten <supervisedthinking@gmail.com>
2019-03-27 13:32:52 +0100

    dolphin: updated to r5.0-9812

--- packages/5schatten/emulation-standalone/dolphin/package.mk
+++ packages/5schatten/emulation-standalone/dolphin/package.mk
@@ -1,15 +1,15 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="dolphin"
-PKG_VERSION="8bad3f93f134efeaecc2483f5ede374064f64563"
-PKG_SHA256="6f4113f0cbc6b73397285d179f605547e998f8baa1b8888437fceae73bfda90f"
-PKG_DOLPHIN_RELEASE="5.0-9806"
+PKG_VERSION="a1f53716dc96fa5eccf1702b4f6e91861bdb407d"
+PKG_SHA256="d23142d431c7c4128f76e8561c0ad18ec2566d22abd4dcd9456ad2c32620a32e"
+PKG_DOLPHIN_RELEASE="5.0-9812"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/dolphin-emu/dolphin"
-PKG_URL="https://github.com/dolphin-emu/dolphin/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/dolphin-emu/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc systemd openal-soft libevdev curl ffmpeg libpng zlib bzip2 bluez pulseaudio alsa-lib libogg libvorbis libSM enet qt-everywhere"
 PKG_LONGDESC="Dolphin is a GameCube / Wii emulator, allowing you to play games for these two platforms on PC with improvements."
 
@@ -23,11 +23,11 @@ pre_configure_target() {
 
 pre_make_target() {
   # fix cross compiling
-  find $PKG_BUILD -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
-  find $PKG_BUILD -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
   
   # Export QT path
-  export Qt5Gui_DIR=$SYSROOT_PREFIX/usr/lib
+  export Qt5Gui_DIR=${SYSROOT_PREFIX}/usr/lib
 
   # Export Dolphin revision
   PKG_DOLPHIN_BRANCH="Master"
@@ -44,12 +44,12 @@ pre_make_target() {
 
 post_makeinstall_target() {
   # Copy scripts & config files
-  mkdir -p $INSTALL/usr/config/dolphin-emu
-  cp $PKG_DIR/scripts/* $INSTALL/usr/bin/
-  cp -PR $PKG_DIR/config/* $INSTALL/usr/config/dolphin-emu/
+  mkdir -p ${INSTALL}/usr/config/dolphin-emu
+  cp ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin/
+  cp -PR ${PKG_DIR}/config/* ${INSTALL}/usr/config/dolphin-emu/
 
   # Clean up
-  rm $INSTALL/usr/bin/dolphin-emu-nogui
-  rm -rf $INSTALL/usr/share/applications
-  rm -rf $INSTALL/usr/share/icons
+  rm ${INSTALL}/usr/bin/dolphin-emu-nogui
+  rm -rf ${INSTALL}/usr/share/applications
+  rm -rf ${INSTALL}/usr/share/icons
 }
