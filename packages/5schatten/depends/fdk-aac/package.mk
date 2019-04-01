6256fad94491545893416f181c65b9881e05da1a
5schatten <supervisedthinking@gmail.com>
2019-04-01 16:36:04 +0200

    fdk-aac: initial package v2.0.0

---
+++ packages/5schatten/depends/fdk-aac/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="fdk-aac"
+PKG_VERSION="2.0.0"
+PKG_SHA256="6e6c7921713788e31df655911e1d42620b057180b00bf16874f5d630e1d5b9a2"
+PKG_LICENSE="other"
+PKG_SITE="https://sourceforge.net/projects/opencore-amr/"
+PKG_URL="https://github.com/mstorsjo/fdk-aac/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="A standalone library of the Fraunhofer FDK AAC code from Android."
+PKG_TOOLCHAIN="autotools"
