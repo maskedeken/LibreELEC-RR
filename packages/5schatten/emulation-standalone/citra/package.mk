5bde777bcfa6640e507ced53705a39b152165c0d
5schatten <supervisedthinking@gmail.com>
2019-04-03 01:13:44 +0200

    citra: updated to git daeba65 / enabled FFmpeg support & added dependency

--- packages/5schatten/emulation-standalone/citra/package.mk
+++ packages/5schatten/emulation-standalone/citra/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="citra"
-PKG_VERSION="c1de8acfe5895f16713cd20cdc105d69036304a3"
+PKG_VERSION="daeba65fb4947e09e1372d41bbe68d8658e30044"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2+"
 PKG_SITE="https://github.com/citra-emu/citra"
 PKG_URL="https://github.com/citra-emu/citra.git"
-PKG_DEPENDS_TARGET="toolchain linux glibc systemd dbus zlib pulseaudio mesa xorg-server boost qt-everywhere SDL2-git"
+PKG_DEPENDS_TARGET="toolchain linux glibc systemd dbus zlib pulseaudio ffmpeg mesa xorg-server boost qt-everywhere SDL2-git"
 PKG_LONGDESC="A Nintendo 3DS Emulator"
 GET_HANDLER_SUPPORT="git"
 
@@ -15,6 +15,7 @@ pre_configure_target() {
   PKG_CMAKE_OPTS_TARGET="-DENABLE_SDL2=1 \
                          -DENABLE_QT=1 \
                          -DENABLE_CUBEB=0 \
+                         -DENABLE_FFMPEG=1 \
                          -DCMAKE_BUILD_TYPE=\"Release\" \
                          -DENABLE_WEB_SERVICE=0 \
                          -DCMAKE_NO_SYSTEM_FROM_IMPORTED=1 \
@@ -23,18 +24,18 @@ pre_configure_target() {
 
 pre_make_target() {
   # fix cross compiling
-  find $PKG_BUILD -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
-  find $PKG_BUILD -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
 }
 
 post_makeinstall_target() {
   # Copy scripts & config files
-  mkdir -p $INSTALL/usr/config/citra-emu
-  cp $PKG_DIR/scripts/*    $INSTALL/usr/bin/
-  cp -PR $PKG_DIR/config/* $INSTALL/usr/config/citra-emu/
-  cp -PR $PKG_DIR/files/*  $INSTALL/usr/config/citra-emu/
+  mkdir -p ${INSTALL}/usr/config/citra-emu
+  cp     ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin/
+  cp -PR ${PKG_DIR}/config/*  ${INSTALL}/usr/config/citra-emu/
+  cp -PR ${PKG_DIR}/files/*   ${INSTALL}/usr/config/citra-emu/
   
   # Clean up
-  rm -rf $INSTALL/usr/share/
-  rm -rf $INSTALL/usr/bin/citra
+  rm -rf ${INSTALL}/usr/share/
+  rm -rf ${INSTALL}/usr/bin/citra
 }
