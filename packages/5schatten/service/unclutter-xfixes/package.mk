9c0ebee66b5f47fd9d6cba8453bf45878854d07b
5schatten <supervisedthinking@gmail.com>
2019-04-02 03:25:43 +0200

    unclutter-xfixes: initial package v1.5

---
+++ packages/5schatten/service/unclutter-xfixes/package.mk
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="unclutter-xfixes"
+PKG_VERSION="1.5"
+PKG_SHA256="35c75ad24be989dd6708db1d9ce9b2a2f814b80638c0633cdb075c6df090ed11"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/Airblader/unclutter-xfixes"
+PKG_URL="https://github.com/Airblader/unclutter-xfixes/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libX11 libXi libXfixes libev-git"
+PKG_LONGDESC="This is a rewrite of the popular tool unclutter, but using the x11-xfixes extension."
+
+post_install() {
+  enable_service unclutter.service
+}
