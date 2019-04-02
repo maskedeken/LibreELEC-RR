041f40bda707d949ec1019e42124c21ac7be9462
5schatten <supervisedthinking@gmail.com>
2019-04-03 00:40:56 +0200

    lr-database: updated to git 0331956 / updated installation

--- packages/5schatten/emulation-frontends/depends-retroarch/lr-database/package.mk
+++ packages/5schatten/emulation-frontends/depends-retroarch/lr-database/package.mk
@@ -1,38 +1,33 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-database"
-PKG_VERSION="d4a13174712598ed41afc50f5c3ba24cd933a3d8"
-PKG_SHA256="de808addeb7c5bff8f492e60d9cbabbe7c85fb2ed989abcf589cec946e57f2e3"
+PKG_VERSION="0331956b9073378bff6995e0b00c5db3c85bf95e"
+PKG_SHA256="5cb48a5bee0149cca8558befee471c628fa5c0940b8a7c3d08d7344e9994d029"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-database"
-PKG_URL="https://github.com/libretro/libretro-database/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/libretro-database/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Repository containing cheatcode files, content data files, etc."
+PKG_LONGDESC="RetroArch database containing cheatcode files, content data files, etc."
 PKG_TOOLCHAIN="manual"
 
-pre_configure_target() {
-  cd ../
-  rm -rf .$TARGET_NAME
-}
-
 makeinstall_target() {
-  make install INSTALLDIR="$INSTALL/usr/share/retroarch/database"
+  cd ${PKG_BUILD}
+  make install INSTALLDIR="${INSTALL}/usr/share/retroarch/database"
 }
 
 post_makeinstall_target() {
   #remove oldest & unneeded MAME 2000 database use mame2003-plus instead
-  rm "$INSTALL/usr/share/retroarch/database/rdb/MAME 2000.rdb"
+  rm "${INSTALL}/usr/share/retroarch/database/rdb/MAME 2000.rdb"
 
   #remove unneeded MAME databases for SBC based systems
   if [ "${PROJECT}" = "Amlogic_Legacy" ] || [ "${PROJECT}" = "RPi" ]; then
-    rm $INSTALL/usr/share/retroarch/database/rdb/MAME.rdb
-    rm $INSTALL/usr/share/retroarch/database/rdb/MAME\ 2015.rdb
+    rm ${INSTALL}/usr/share/retroarch/database/rdb/MAME.rdb
+    rm ${INSTALL}/usr/share/retroarch/database/rdb/MAME\ 2015.rdb
   fi
 
   #workaround until a MAME 2016 database for romset 0.174 is included
-  if [ ! -f "$INSTALL/usr/share/retroarch/database/rdb/MAME 2016.rdb" ] && [ "${PROJECT}" = "Generic" ]; then
-    ln -sf /usr/share/retroarch/database/rdb/MAME.rdb "$INSTALL/usr/share/retroarch/database/rdb/MAME 2016.rdb"
+  if [ ! -f "${INSTALL}/usr/share/retroarch/database/rdb/MAME 2016.rdb" ] && [ "${PROJECT}" = "Generic" ]; then
+    ln -sf /usr/share/retroarch/database/rdb/MAME.rdb "${INSTALL}/usr/share/retroarch/database/rdb/MAME 2016.rdb"
   fi
 }
