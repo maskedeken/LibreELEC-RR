1a8ba5466d1183e647cd1459eafa25556006202e
5schatten <supervisedthinking@gmail.com>
2019-04-03 00:41:33 +0200

    lr-mesen: updated to git 96851e6

--- packages/5schatten/emulation-lr-cores/lr-mesen/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-mesen/package.mk
@@ -2,22 +2,22 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-mesen"
-PKG_VERSION="cf40b967f438684cc5784d0cf41df11580df201b" # 0.9.7+
-PKG_SHA256="1583c31808f0dea4b431a0209a451d736046ec6c8c60f2dfd2f519aa75eeb17f"
+PKG_VERSION="96851e6c7a881e40322a1a3e33b5aa4fcc0acbce" # 0.9.7+
+PKG_SHA256="c335083fc2143cbbefacc28726cd8ecc3699cbf11b9563332d357084cfebc18f"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/SourMesen/Mesen"
-PKG_URL="https://github.com/SourMesen/Mesen/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/SourMesen/Mesen/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Mesen is a cross-platform (Windows & Linux) NES/Famicom emulator built in C++ and C#"
 PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="mesen_libretro.so"
-PKG_LIBPATH="Libretro/$PKG_LIBNAME"
+PKG_LIBPATH="Libretro/${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="-C Libretro"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
