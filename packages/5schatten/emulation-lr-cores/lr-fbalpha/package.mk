c575dcada10f509eddad0ad48bf79d78ff25c76e
5schatten <supervisedthinking@gmail.com>
2019-04-03 00:41:15 +0200

    lr-fbalpha: updated to git e09fed1

--- packages/5schatten/emulation-lr-cores/lr-fbalpha/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-fbalpha/package.mk
@@ -2,23 +2,23 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-fbalpha"
-PKG_VERSION="a995bc53d890df7499c931cfa3d0a29779a902cd"
-PKG_SHA256="3266261a035292eadf106e12f51930af7085737f9206828c87c24c6361562a0f"
+PKG_VERSION="e09fed14ad6010668960055cecf56fe3c33d88ec"
+PKG_SHA256="462482ba5f6c39f7afeb8db4b0632fa01bf24fb0cb29df6d934af2fba0d0dd73"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/fbalpha"
-PKG_URL="https://github.com/libretro/fbalpha/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/fbalpha/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
-PKG_LONGDESC="Port of Final Burn Alpha to Libretro (v0.2.97.44)."
+PKG_LONGDESC="A libretro port of Final Burn Alpha for Romset v0.2.97.44"
 PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="fbalpha_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="-f makefile.libretro GIT_VERSION=${PKG_VERSION:0:7}"
 
 pre_configure_target() {
-  if [ "$PROJECT" = "RPi" ]; then
-    case $DEVICE in
+  if [ "${PROJECT}" = "RPi" ]; then
+    case ${DEVICE} in
       RPi)
         PKG_MAKE_OPTS_TARGET+=" platform=armv"
         ;;
@@ -35,6 +35,6 @@ pre_configure_target() {
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
