0a780622d9befb40478fc293f05d675c22a92c4e
5schatten <supervisedthinking@gmail.com>
2019-04-06 12:50:57 +0200

    lr-mgba: updated to git bf8c1d1

--- packages/5schatten/emulation-lr-cores/lr-mgba/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-mgba/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-mgba"
-PKG_VERSION="45ec6e50e23532b02c492657f781c26221698464" # v0.7.1+
-PKG_SHA256="c60de9feae38a7f81e25a0b8463556b6c6868b6a5654b125493aa78e83e540f2"
+PKG_VERSION="bf8c1d1b4b52d45cd84590dc7bd7d40b0dffc7bd" # v0.7.1+
+PKG_SHA256="a221c8ab7467470941211989865a4c6d0b340595702e16715832bee4b257a6a9"
 PKG_LICENSE="MPL 2.0"
 PKG_SITE="https://github.com/mgba-emu/mgba"
 PKG_URL="https://github.com/mgba-emu/mgba/archive/$PKG_VERSION.tar.gz"
