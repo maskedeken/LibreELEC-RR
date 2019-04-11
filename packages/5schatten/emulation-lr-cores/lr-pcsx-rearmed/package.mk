517499f6ea778c2760ccdaba32af451fcbaf5f10
5schatten <supervisedthinking@gmail.com>
2019-04-11 23:28:29 +0200

    lr-pcsx-rearmed: updated to git e1d8eb0

--- packages/5schatten/emulation-lr-cores/lr-pcsx-rearmed/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-pcsx-rearmed/package.mk
@@ -2,23 +2,23 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-pcsx-rearmed"
-PKG_VERSION="588bf7918460109815d5cc505ada4334a4c5a791"
-PKG_SHA256="3416ca1e2d3a04a703f809967fe3bf66750dd8a64647d5ee3bc590d920bd70f7"
+PKG_VERSION="e1d8eb0f131f9e7e838ec5658345e51ebcc3179c"
+PKG_SHA256="6968c96eae505c92aae47110a1f1fd4f82876706e23985cd0922fd64b385ddf1"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/pcsx_rearmed"
-PKG_URL="https://github.com/libretro/pcsx_rearmed/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/pcsx_rearmed/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="PCSX ReARMed is yet another PCSX fork based on the PCSX-Reloaded project, which itself contains code from PCSX, PCSX-df and PCSX-Revolution."
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="-gold"
 
 PKG_LIBNAME="pcsx_rearmed_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="-f Makefile.libretro GIT_VERSION=${PKG_VERSION:0:7}"
 
 pre_configure_target() {
-  cd $PKG_BUILD
+  cd ${PKG_BUILD}
   if [ "${ARCH}" = "arm" ]; then
     PKG_MAKE_OPTS_TARGET+=" USE_DYNAREC=1"
 
@@ -31,6 +31,6 @@ pre_configure_target() {
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
