0ffd380c5bb2f9cdfec6fe9383c2670d354aa098
5schatten <supervisedthinking@gmail.com>
2019-04-11 19:20:25 +0200

    vulkan-tools: updated to v1.1.106

--- packages/5schatten/sysutils/vulkan-tools/package.mk
+++ packages/5schatten/sysutils/vulkan-tools/package.mk
@@ -2,12 +2,11 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="vulkan-tools"
-PKG_VERSION="97e866a6d3cd3e9851d13df1ec524c14aca77436" # 1.1.102+
-PKG_SHA256="049800f302d0110dfe9d3ad295d78c0e44417fa3d41473614da0788f3f6639ba"
-PKG_ARCH="x86_64"
+PKG_VERSION="1.1.106"
+PKG_SHA256="fbfdf520f50ff82621671ee028ebfe9db2052223b64f0d2627156dacfe795ed5"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Tools"
-PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain cmake:host vulkan-loader"
 PKG_LONGDESC="This project provides Vulkan tools and utilities that can assist development by enabling developers to verify their applications correct use of the Vulkan API."
 
