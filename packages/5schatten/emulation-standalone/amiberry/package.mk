44379434b5116f69a65ac435ce3380f308443ac7
5schatten <supervisedthinking@gmail.com>
2019-04-11 21:12:12 +0200

    amiberry: updated to git 99d1b64 / updated amlogic legacy platform patch / reworked build

--- packages/5schatten/emulation-standalone/amiberry/package.mk
+++ packages/5schatten/emulation-standalone/amiberry/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="amiberry"
-PKG_VERSION="28fc0775172c2d47486cd35d33698ad13246a9f5" # v2.25+
+PKG_VERSION="99d1b64852c770dce70b05a44547ac3c712cae57" # v2.25+
 PKG_ARCH="arm"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/midwan/amiberry"
@@ -15,45 +15,53 @@ PKG_TOOLCHAIN="make"
 PKG_MAKE_OPTS_TARGET="all"
 
 pre_configure_target() {
-  cd $PKG_BUILD
-  export SYSROOT_PREFIX=$SYSROOT_PREFIX
+  cd ${PKG_BUILD}
+  export SYSROOT_PREFIX=${SYSROOT_PREFIX}
 
-  # build amiberry
-  if [ "${PROJECT}" = "Amlogic_Legacy" ]; then
-    PKG_MAKE_OPTS_TARGET+=" PLATFORM=aml"
-  elif [ "${PROJECT}" = "RPi" ]; then
-    case ${DEVICE} in
-      RPi)
-      PKG_MAKE_OPTS_TARGET+=" PLATFORM=rpi1-sdl2"
-        ;;
-      RPi2)
-      PKG_MAKE_OPTS_TARGET+=" PLATFORM=rpi2-sdl2"
-        ;;
-    esac
-  fi
+  case ${PROJECT} in
+    Amlogic_Legacy)
+      AMIBERRY_PLATFORM="amlogic_legacy"
+      ;;
+    Rockchip)
+      if [ "${DEVICE}" = "RK3399" ]; then
+        AMIBERRY_PLATFORM="rockpro64"
+      else
+        AMIBERRY_PLATFORM="tinker"
+      fi
+      ;;
+    RPi)
+      if [ "${DEVICE}" = "RPi2" ]; then
+        AMIBERRY_PLATFORM="rpi2-sdl2"
+      else
+        AMIBERRY_PLATFORM="rpi1-sdl2"
+      fi
+      ;;
+  esac
+
+  PKG_MAKE_OPTS_TARGET+=" PLATFORM=${AMIBERRY_PLATFORM}"
 }
 
 makeinstall_target() {
   # Create directories
-  mkdir -p $INSTALL/usr/bin
-  mkdir -p $INSTALL/usr/lib
-  mkdir -p $INSTALL/usr/config/amiberry
-  mkdir -p $INSTALL/usr/config/amiberry/controller
+  mkdir -p ${INSTALL}/usr/bin
+  mkdir -p ${INSTALL}/usr/lib
+  mkdir -p ${INSTALL}/usr/config/amiberry
+  mkdir -p ${INSTALL}/usr/config/amiberry/controller
 
   # Copy ressources
-  cp -R $PKG_DIR/config/* $INSTALL/usr/config/amiberry
-  cp -R data $INSTALL/usr/config/amiberry/
-  ln -s /storage/roms/bios/Kickstarts $INSTALL/usr/config/amiberry/kickstarts
-  cp -R savestates $INSTALL/usr/config/amiberry/
-  cp -R screenshots $INSTALL/usr/config/amiberry/
-  cp -R whdboot $INSTALL/usr/config/amiberry/
+  cp -a ${PKG_DIR}/config/*           ${INSTALL}/usr/config/amiberry/
+  cp -a data                          ${INSTALL}/usr/config/amiberry/
+  cp -a savestates                    ${INSTALL}/usr/config/amiberry/
+  cp -a screenshots                   ${INSTALL}/usr/config/amiberry/
+  cp -a whdboot                       ${INSTALL}/usr/config/amiberry/
+  ln -s /storage/roms/bios/Kickstarts ${INSTALL}/usr/config/amiberry/kickstarts
 
   # Create links to Retroarch controller files
-  ln -s /usr/share/retroarch/autoconfig/udev/8Bitdo_Pro_SF30_BT_B.cfg "$INSTALL/usr/config/amiberry/controller/8Bitdo SF30 Pro.cfg"
-  ln -s "/usr/share/retroarch/autoconfig/udev/Pro Controller.cfg" "$INSTALL/usr/config/amiberry/controller/Pro Controller.cfg"
+  ln -s /usr/share/retroarch/autoconfig/udev/8Bitdo_Pro_SF30_BT_B.cfg "${INSTALL}/usr/config/amiberry/controller/8Bitdo SF30 Pro.cfg"
+  ln -s "/usr/share/retroarch/autoconfig/udev/Pro Controller.cfg"     "${INSTALL}/usr/config/amiberry/controller/Pro Controller.cfg"
 
   # Copy binary, scripts & link libcapsimg
-  cp amiberry-*-sdl2 $INSTALL/usr/bin/amiberry
-  cp $PKG_DIR/scripts/* $INSTALL/usr/bin
-  ln -sf /usr/lib/libcapsimage.so.5.1 $INSTALL/usr/config/amiberry/capsimg.so
+  cp -a amiberry-${AMIBERRY_PLATFORM} ${INSTALL}/usr/bin/amiberry
+  cp -a ${PKG_DIR}/scripts/*          ${INSTALL}/usr/bin
+  ln -sf /usr/lib/libcapsimage.so.5.1 ${INSTALL}/usr/config/amiberry/capsimg.so
 }
