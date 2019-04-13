fc97c0179c1974f2757d686e81f0628d20ca431d
5schatten <supervisedthinking@gmail.com>
2019-04-13 20:55:14 +0200

    lr-gambatte: updated to git 7d4beec

--- packages/5schatten/emulation-lr-cores/lr-gambatte/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-gambatte/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-gambatte"
-PKG_VERSION="611cbfd743615a22b569dfb27c7cf805041d719a"
-PKG_SHA256="c9d2e981d04b0b44e3f2b72dfd5bdabdc27369d0c8529d2201d2d809dfbfe3a6"
+PKG_VERSION="7d4beec1ff69098a602c12d38a76465510cb8ebc"
+PKG_SHA256="ed8b68ac1d7378366818a5887afcf8cf83f1d00ed572852becd9e78b62baa90c"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/gambatte-libretro"
 PKG_URL="https://github.com/libretro/gambatte-libretro/archive/$PKG_VERSION.tar.gz"
