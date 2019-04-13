9f3edd8bb4f47857ef9633291d0d81a8e4fac019
5schatten <supervisedthinking@gmail.com>
2019-04-13 16:35:33 +0200

    libmali: switched repo / updated to git 9072d13 / dropped patch

--- packages/graphics/libmali/package.mk
+++ packages/graphics/libmali/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libmali"
-PKG_VERSION="74c27b4b401e3266c71ab3718ef9878bd36dbd34"
-PKG_SHA256="3a4d9295a5539fcaa470a5f0cdeda794be604842ff2c2936f2aab33ccf50bc2d"
+PKG_VERSION="9072d13ffaf1e3df57bf7ee902b9d4fe88d912d6"
+PKG_SHA256="5584becb912b1a783844b4d3ecca7c2dd8118ffddc82f0c9f8683b8adaa19b2c"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/LibreELEC/libmali"
-PKG_URL="https://github.com/LibreELEC/libmali/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/5schatten/libmali/archive/$PKG_VERSION.tar.gz"
 PKG_LONGDESC="OpenGL ES user-space binary for the ARM Mali GPU family"
 
 PKG_DEPENDS_TARGET="libdrm"
