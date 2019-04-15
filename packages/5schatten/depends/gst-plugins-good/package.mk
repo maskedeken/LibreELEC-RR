113a4f91c9e3befa89d958b0e757c8d34f4c6f74
5schatten <supervisedthinking@gmail.com>
2019-04-15 19:06:03 +0200

    gst-plugins-good: updated to v1.15.90 / dropped glib patch

--- packages/5schatten/depends/gst-plugins-good/package.mk
+++ packages/5schatten/depends/gst-plugins-good/package.mk
@@ -2,20 +2,21 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="gst-plugins-good"
-PKG_VERSION="1.14.4"
-PKG_SHA256="5f8b553260cb0aac56890053d8511db1528d53cae10f0287cfce2cb2acc70979"
+PKG_VERSION="1.15.90"
+PKG_SHA256="23330ddba08177f6b279b38e0d88c10a5af5ca4b5550e5fd70a7f0d9db6b086a"
 PKG_LICENSE="GPL"
 PKG_SITE="https://gstreamer.freedesktop.org/modules/gst-plugins-good.html"
-PKG_URL="https://gstreamer.freedesktop.org/src/gst-plugins-good/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="https://gstreamer.freedesktop.org/src/gst-plugins-good/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain gstreamer gst-plugins-base"
 PKG_LONGDESC="Good GStreamer plugins and helper libraries"
-PKG_TOOLCHAIN="configure"
 
-pre_configure_target() {
-  PKG_CONFIGURE_OPTS_TARGET="--disable-vpx"
-}
+PKG_MESON_OPTS_TARGET="-Dgdk-pixbuf=disabled \
+                       -Dtaglib=disabled \
+                       -Dexamples=disabled \
+                       -Dtests=disabled \
+                       -Dnls=disabled"
 
 post_makeinstall_target(){
   # Clean up
-  rm -rf $INSTALL/usr/share
+  rm -rf ${INSTALL}/usr/share
 }
