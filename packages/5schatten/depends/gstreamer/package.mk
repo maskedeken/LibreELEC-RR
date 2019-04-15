55a21c925eaa42b6cfc4a57f27c90d75e27e20ef
5schatten <supervisedthinking@gmail.com>
2019-04-15 19:05:33 +0200

    gstreamer: updated to v1.15.90 / dropped glib patch

--- packages/5schatten/depends/gstreamer/package.mk
+++ packages/5schatten/depends/gstreamer/package.mk
@@ -2,14 +2,16 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="gstreamer"
-PKG_VERSION="1.14.4"
-PKG_SHA256="f94f6696c5f05a3b3a9183e39c5f5c0b779f75a04c0efa497e7920afa985ffc7"
+PKG_VERSION="1.15.90"
+PKG_SHA256="f71fe61f6b90b5a1ded99d5e5a47b90f2855dbcad115863d4356a9d330f6bbb6"
 PKG_LICENSE="GPL"
 PKG_SITE="https://gstreamer.freedesktop.org"
-PKG_URL="https://gstreamer.freedesktop.org/src/gstreamer/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="https://gstreamer.freedesktop.org/src/gstreamer/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="GStreamer open-source multimedia framework core library"
 
-PKG_MESON_OPTS_TARGET="-Ddisable_gtkdoc=true \
-                       -Ddisable_examples=true \
-                       -Ddisable_libunwind=true"
+PKG_MESON_OPTS_TARGET="-Dlibunwind=disabled \
+                       -Dgtk_doc=disabled \
+                       -Dexamples=disabled \
+                       -Dtests=disabled \
+                       -Dnls=disabled"
