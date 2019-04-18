3b4ec79128396aee38d4a579ca41b032fc059b56
5schatten <supervisedthinking@gmail.com>
2019-04-18 21:47:04 +0200

    SDL_GameControllerDB: updated to git cbebd79

--- packages/5schatten/depends/SDL_GameControllerDB/package.mk
+++ packages/5schatten/depends/SDL_GameControllerDB/package.mk
@@ -2,16 +2,16 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="SDL_GameControllerDB"
-PKG_VERSION="4f9962c7b268e32fa9a2a96308cfdf66795e782a"
-PKG_SHA256="db4e8777127dac84a09e37faa79cd9db2b6a1b7e557c94a08c42977d68f39cc9"
+PKG_VERSION="cbebd79740bc1360978bdf6d9d4d13d189e420a3"
+PKG_SHA256="a1274ed0acf6cf4c8596d1a55a240db686d86ee346d896fdb8bc8c10c2a27415"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/gabomdq/SDL_GameControllerDB"
-PKG_URL="https://github.com/gabomdq/SDL_GameControllerDB/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/gabomdq/SDL_GameControllerDB/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET=""
 PKG_LONGDESC="A community sourced database of game controller mappings to be used with SDL2 Game Controller functionality"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/config/SDL-GameControllerDB
-  cp $PKG_BUILD/gamecontrollerdb.txt $INSTALL/usr/config/SDL-GameControllerDB
+  mkdir -p ${INSTALL}/usr/config/SDL-GameControllerDB
+  cp ${PKG_BUILD}/gamecontrollerdb.txt ${INSTALL}/usr/config/SDL-GameControllerDB
 }
