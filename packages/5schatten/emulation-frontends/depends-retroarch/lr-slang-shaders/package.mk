ed0b180d3c2250448be85439082d458706fec8a8
5schatten <supervisedthinking@gmail.com>
2019-04-18 21:51:02 +0200

    lr-slang-shaders: updated to git 77d4efd

--- packages/5schatten/emulation-frontends/depends-retroarch/lr-slang-shaders/package.mk
+++ packages/5schatten/emulation-frontends/depends-retroarch/lr-slang-shaders/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-slang-shaders"
-PKG_VERSION="04081a285bde40998e8dfff99070f0d0607abff2"
-PKG_SHA256="b2aff0d405fcc11c99efa7529780422f496e81d37004e62bc61bb6b267fe035a"
+PKG_VERSION="77d4efd688b7d9243226da08df22b51c787e023d"
+PKG_SHA256="d81a4f6b03632651ff5763563cba563c9e7a6653a318f0cb5ce03e70bdbb46ab"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/slang-shaders"
-PKG_URL="https://github.com/libretro/slang-shaders/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/slang-shaders/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Vulkan GLSL RetroArch shader system"
 PKG_TOOLCHAIN="manual"
