03c998deb6221069ea3c2370e5504ff6fa166811
5schatten <supervisedthinking@gmail.com>
2019-04-19 15:01:35 +0200

    GLideN64: downgrade to ae025e1

--- packages/5schatten/emulation-standalone/m64p/depends-m64p/GLideN64/package.mk
+++ packages/5schatten/emulation-standalone/m64p/depends-m64p/GLideN64/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
 
 PKG_NAME="GLideN64"
-PKG_VERSION="8f266454074bb5b5f6142272eeb8a56899776604"
-PKG_SHA256="3cd57d2cf0c8088ecba3dfe3ab4cc3af7f98d6ea422d50caecd63705f06a3c94"
+PKG_VERSION="ae025e10ea7e4084628894b09e4a20a30bae0c0b"
+PKG_SHA256="8d1f90ad1e1bd57b1593971aada162670f828c6f49b7b8cbcb309d46efc93dd9"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/gonetz/GLideN64"
-PKG_URL="https://github.com/gonetz/GLideN64/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/gonetz/GLideN64/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc freetype:host zlib bzip2 libpng"
 PKG_LONGDESC="A new generation, open-source graphics plugin for N64 emulators."
 PKG_TOOLCHAIN="cmake"
@@ -34,17 +34,17 @@ configure_package() {
 }
 
 pre_configure_target() {
-  PKG_CMAKE_SCRIPT="$PKG_BUILD/src/CMakeLists.txt"
+  PKG_CMAKE_SCRIPT="${PKG_BUILD}/src/CMakeLists.txt"
   PKG_CMAKE_OPTS_TARGET="-DUSE_SYSTEM_LIBS=On \
                          -DVEC4_OPT=On \
                          -DCRC_OPT=On \
                          -DMUPENPLUSAPI=On"
   # Fix revision header
-  PKG_REV_H=$PKG_BUILD/src/Revision.h
+  PKG_REV_H=${PKG_BUILD}/src/Revision.h
   echo "#define PLUGIN_REVISION" ""\"${PKG_VERSION:0:10}""\"     > ${PKG_REV_H}
   echo "#define PLUGIN_REVISION_W" "L"\"${PKG_VERSION:0:10}""\" >> ${PKG_REV_H}
 
-  rm -rf $PKG_BUILD/src/GLideNHQ/inc
+  rm -rf ${PKG_BUILD}/src/GLideNHQ/inc
 
   # NEON Support
   if target_has_feature neon; then
