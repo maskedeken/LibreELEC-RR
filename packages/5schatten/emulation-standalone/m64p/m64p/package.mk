ceac3368b3e810bcd5f670d0f6ea8eb8668a41a6
5schatten <supervisedthinking@gmail.com>
2019-04-20 11:06:58 +0200

    m64p: install libs verbose

--- packages/5schatten/emulation-standalone/m64p/m64p/package.mk
+++ packages/5schatten/emulation-standalone/m64p/m64p/package.mk
@@ -18,31 +18,31 @@ configure_package() {
 
 makeinstall_target() {
   # Create directories
-  mkdir -p $INSTALL/usr/bin
-  mkdir -p $INSTALL/usr/config/mupen64plus
-  mkdir -p $INSTALL/usr/lib/mupen64plus
+  mkdir -p ${INSTALL}/usr/bin
+  mkdir -p ${INSTALL}/usr/config/mupen64plus
+  mkdir -p ${INSTALL}/usr/lib/mupen64plus
 
   if [ "${PROJECT}" = "Generic" ]; then
-    cp $(get_build_dir angrylion-rdp-plus)/.${TARGET_NAME}/*.so         $INSTALL/usr/lib/mupen64plus
-    cp $(get_build_dir mupen64plus-gui)/.${TARGET_NAME}/mupen64plus-gui $INSTALL/usr/bin
-    cp $PKG_DIR/scripts/gui/*                                           $INSTALL/usr/bin
+    cp $(get_build_dir angrylion-rdp-plus)/.${TARGET_NAME}/*.so         ${INSTALL}/usr/lib/mupen64plus
+    cp $(get_build_dir mupen64plus-gui)/.${TARGET_NAME}/mupen64plus-gui ${INSTALL}/usr/bin
+    cp ${PKG_DIR}/scripts/gui/*                                         ${INSTALL}/usr/bin
   fi
 
   # Install binaries & scripts
-  cp $(get_build_dir mupen64plus-ui-console)/mupen64plus $INSTALL/usr/bin/mupen64plus-cli
-  cp $PKG_DIR/scripts/cli/*                              $INSTALL/usr/bin
+  cp $(get_build_dir mupen64plus-ui-console)/mupen64plus ${INSTALL}/usr/bin/mupen64plus-cli
+  cp ${PKG_DIR}/scripts/cli/*                            ${INSTALL}/usr/bin
 
   # Install config files
-  cp $PKG_DIR/config/*                             $INSTALL/usr/config/mupen64plus
-  cp $(get_build_dir GLideN64)/ini/*.ini           $INSTALL/usr/config/mupen64plus
-  cp $(get_build_dir mupen64plus-core)/data/*      $INSTALL/usr/config/mupen64plus
-  cp $(get_build_dir mupen64plus-input-sdl)/data/* $INSTALL/usr/config/mupen64plus
+  cp ${PKG_DIR}/config/*                             ${INSTALL}/usr/config/mupen64plus
+  cp $(get_build_dir GLideN64)/ini/*.ini             ${INSTALL}/usr/config/mupen64plus
+  cp $(get_build_dir mupen64plus-core)/data/*        ${INSTALL}/usr/config/mupen64plus
+  cp $(get_build_dir mupen64plus-input-sdl)/data/*   ${INSTALL}/usr/config/mupen64plus
 
   # Install libs
-  cp $(get_build_dir GLideN64)/plugin/Release/*.so $INSTALL/usr/lib/mupen64plus
-  cp $(get_build_dir mupen64plus-audio-sdl2)/*.so  $INSTALL/usr/lib/mupen64plus
-  cp -P $(get_build_dir mupen64plus-core)/*.so*    $INSTALL/usr/lib/mupen64plus
-  cp $(get_build_dir mupen64plus-input-sdl)/*.so   $INSTALL/usr/lib/mupen64plus
-  cp $(get_build_dir mupen64plus-rsp-cxd4)/*.so    $INSTALL/usr/lib/mupen64plus
-  cp $(get_build_dir mupen64plus-rsp-hle)/*.so     $INSTALL/usr/lib/mupen64plus
+  cp -v $(get_build_dir GLideN64)/plugin/*/*.so       ${INSTALL}/usr/lib/mupen64plus
+  cp -v $(get_build_dir mupen64plus-audio-sdl2)/*.so  ${INSTALL}/usr/lib/mupen64plus
+  cp -Pv $(get_build_dir mupen64plus-core)/*.so*      ${INSTALL}/usr/lib/mupen64plus
+  cp -v $(get_build_dir mupen64plus-input-sdl)/*.so   ${INSTALL}/usr/lib/mupen64plus
+  cp -v $(get_build_dir mupen64plus-rsp-cxd4)/*.so    ${INSTALL}/usr/lib/mupen64plus
+  cp -v $(get_build_dir mupen64plus-rsp-hle)/*.so     ${INSTALL}/usr/lib/mupen64plus
 }
