1dbece3d72c5da2ccc18d89feea719611d5631ce
5schatten <supervisedthinking@gmail.com>
2019-04-21 22:49:57 +0200

    enet: updated to v1.3.14 pre-release / reworked package

--- packages/addons/addon-depends/enet/package.mk
+++ packages/addons/addon-depends/enet/package.mk
@@ -2,21 +2,19 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="enet"
-PKG_VERSION="39a72ab"
-PKG_SHA256="fc9b0eaf3f4d5a20ad138d8414a9586ca9ab5e8f44b33d9de4eacb40bbe07f0d"
-PKG_ARCH="any"
-PKG_LICENSE=""
+PKG_VERSION="0eaf48eeb0d94a18d079378d8b76d588832ce838"
+PKG_SHA256="f7c29eef161414f7f2c2191d256b03c191c370961a9960072858fa7feed8590e"
+PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/lsalzman/enet"
-PKG_URL="https://github.com/lsalzman/enet/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/lsalzman/enet/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="network"
-PKG_SHORTDESC="ENet's purpose is to provide a relatively thin, simple and robust network communication layer on top of UDP (User Datagram Protocol)"
 PKG_LONGDESC="ENet's purpose is to provide a relatively thin, simple and robust network communication layer on top of UDP (User Datagram Protocol)"
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
+PKG_CONFIGURE_OPTS_TARGET="--enable-static \
+                           --disable-shared"
 
 post_makeinstall_target() {
-  rm -r $INSTALL
+  rm -r ${INSTALL}
 }
