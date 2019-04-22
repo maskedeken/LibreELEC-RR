425a4ffdcf63bc77c9c918fd18e0a10fbb9d1531
5schatten <supervisedthinking@gmail.com>
2019-04-23 01:03:49 +0200

    lr-glsl-shaders: updated to git 08202e7

--- packages/5schatten/emulation-frontends/depends-retroarch/lr-glsl-shaders/package.mk
+++ packages/5schatten/emulation-frontends/depends-retroarch/lr-glsl-shaders/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
 
 PKG_NAME="lr-glsl-shaders"
-PKG_VERSION="0ec3c790d3627e2a1debb6eae2c40ef7e571a6f4"
-PKG_SHA256="736fe3911759a8dc48d9d2fffd445bce72fc3c5d9676a78097c04f2101909be3"
+PKG_VERSION="08202e7e14c20b4e303f7e96d89c6490e6a86355"
+PKG_SHA256="e015aeb6e0e56f34c6fa685bb28792c28f1f357eeca95a2b73e84cfe69a41cc5"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/glsl-shaders"
 PKG_URL="https://github.com/libretro/glsl-shaders/archive/${PKG_VERSION}.tar.gz"
