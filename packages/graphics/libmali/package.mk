5aa3ff83e8d6427a0077baebdd52c64199ece49e
Jonas Karlman <jonas@kwiboo.se>
2019-04-28 18:02:13 +0000

    libmali: update to 28f97bf
    Adds support for multiple so-files

--- packages/graphics/libmali/package.mk
+++ packages/graphics/libmali/package.mk
@@ -2,13 +2,14 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libmali"
-PKG_VERSION="ad56ed30985471c0950a654cc9db1e86310650d5"
-PKG_SHA256="72438ea73cf6c2e8e770545872386b66ccad200c568261e5304845547099c9ed"
+PKG_VERSION="28f97bf45f5e71cf760b6d8a7abba231998c68c1"
+PKG_SHA256="bf4de370efb2ead9153594a617430611af5b6801a93f6825b9ede2b7f5692cd6"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/LibreELEC/libmali"
 PKG_URL="https://github.com/LibreELEC/libmali/archive/$PKG_VERSION.tar.gz"
 PKG_LONGDESC="OpenGL ES user-space binary for the ARM Mali GPU family"
+PKG_STAMP="$MALI_FAMILY"
 
 PKG_DEPENDS_TARGET="libdrm"
 
@@ -18,10 +19,19 @@ fi
 
 PKG_CMAKE_OPTS_TARGET="-DMALI_VARIANT=$MALI_FAMILY"
 
-if [ -n "$MALI_REVISION" ]; then
-  PKG_CMAKE_OPTS_TARGET+=" -DMALI_REVISION=$MALI_REVISION"
-fi
-
 if [ "$TARGET_ARCH" = "aarch64" ]; then
   PKG_CMAKE_OPTS_TARGET+=" -DMALI_ARCH=aarch64-linux-gnu"
 fi
+
+post_makeinstall_target() {
+  mkdir -p $INSTALL/usr/bin
+    cp -v $PKG_DIR/scripts/libmali-setup $INSTALL/usr/bin
+
+  if [ $(ls -1q $INSTALL/usr/lib/libmali-*.so | wc -l) -gt 1 ]; then
+    ln -sfv /var/lib/libmali/libmali.so $INSTALL/usr/lib/libmali.so
+  fi
+}
+
+post_install() {
+  enable_service libmali-setup.service
+}
