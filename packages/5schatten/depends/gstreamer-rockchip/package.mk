86de88f9fb6471aab53cef2713d9bcd449ca812c
5schatten <supervisedthinking@gmail.com>
2019-04-28 14:34:25 +0200

    gstreamer-rockchip: initial package git c766879

---
+++ packages/5schatten/depends/gstreamer-rockchip/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+
+PKG_NAME="gstreamer-rockchip"
+PKG_VERSION="c766879e0b750233b197a2dd495e562d64498244"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/rockchip-linux/gstreamer-rockchip"
+PKG_URL="https://github.com/rockchip-linux/gstreamer-rockchip.git"
+PKG_DEPENDS_TARGET="toolchain gstreamer gst-plugins-base"
+PKG_LONGDESC="The Gstreamer hardware encoder/decoder plugins for Rockchip platform."
+GET_HANDLER_SUPPORT="git"
+PKG_TOOLCHAIN="autotools"
