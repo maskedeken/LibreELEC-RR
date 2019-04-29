81e1b1a31d8e240fed39b6d91d841b7c2218968b
5schatten <supervisedthinking@gmail.com>
2019-04-29 13:00:01 +0200

    vulkan-headers: updated to v 1.1.107

--- packages/5schatten/drivers/vulkan-headers/package.mk
+++ packages/5schatten/drivers/vulkan-headers/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
 
 PKG_NAME="vulkan-headers"
-PKG_VERSION="1.1.106"
-PKG_SHA256="42625612d14bb920f0d88361dea01d2e839abba1c3d1515ffd3be0946d4668a4"
+PKG_VERSION="1.1.107"
+PKG_SHA256="7d00748f3311a89d5a92aea4b11764bb8371dae4ab6291497bcde2fa186bbd31"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Headers"
-PKG_URL="https://github.com/KhronosGroup/Vulkan-Headers/archive/v$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/KhronosGroup/Vulkan-Headers/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain cmake:host"
 PKG_LONGDESC="Vulkan Header files and API registry"
