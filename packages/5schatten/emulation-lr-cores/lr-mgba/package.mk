9c3cc4fd011444e8fa05c033781ab7aa0a473a35
5schatten <supervisedthinking@gmail.com>
2019-04-30 21:18:32 +0200

    lr-mgba: updated to git 1deff07

--- packages/5schatten/emulation-lr-cores/lr-mgba/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-mgba/package.mk
@@ -2,16 +2,16 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
 
 PKG_NAME="lr-mgba"
-PKG_VERSION="bf8c1d1b4b52d45cd84590dc7bd7d40b0dffc7bd" # v0.7.1+
-PKG_SHA256="a221c8ab7467470941211989865a4c6d0b340595702e16715832bee4b257a6a9"
+PKG_VERSION="1deff07aa1b0d3d4b6b15e875fbeb0cc8eff0926" # v0.7.1+
+PKG_SHA256="97ddd736e88a11974496310ce313ac34dafc0cec5898d10a4b7021a8ef459318"
 PKG_LICENSE="MPL 2.0"
 PKG_SITE="https://github.com/mgba-emu/mgba"
-PKG_URL="https://github.com/mgba-emu/mgba/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/mgba-emu/mgba/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc zlib"
 PKG_LONGDESC="mGBA is an emulator for running Game Boy Advance games."
 
 PKG_LIBNAME="mgba_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 
 configure_package() {
   if [ "${OPENGL_SUPPORT}" = "yes" -a "${PROJECT}" = "Generic" ]; then
@@ -30,6 +30,7 @@ pre_configure_target() {
                          -DUSE_MINIZIP=OFF \
                          -DUSE_LIBZIP=OFF \
                          -DUSE_MAGICK=OFF \
+                         -DUSE_DISCORD_RPC=OFF \
                          -DUSE_ELF=OFF"
 
   if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
@@ -42,6 +43,6 @@ pre_make_target() {
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
