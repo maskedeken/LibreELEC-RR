4487c67fdccbf1f2fe9cddfbfeef78aed198838c
5schatten <supervisedthinking@gmail.com>
2019-04-30 21:17:25 +0200

    retroarch-joypad-autoconfig: updated to git 5b6e463

--- packages/5schatten/emulation-frontends/depends-retroarch/retroarch-joypad-autoconfig/package.mk
+++ packages/5schatten/emulation-frontends/depends-retroarch/retroarch-joypad-autoconfig/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="012505b5fbaf48947137c7b104d66972ac9a94ac"
-PKG_SHA256="0afe3baec775f66b43101c55e37d2833bff5e2e9f8900524b9c94fc60d7774dd"
+PKG_VERSION="5b6e463c5defea48e64a21b22785eec334881a7d"
+PKG_SHA256="da660aa4d12bebbba1c1a9c7623c85087eeb72e57a189098f6dd79d9d65ba8cd"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/retroarch-joypad-autoconfig"
-PKG_URL="https://github.com/libretro/retroarch-joypad-autoconfig/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/retroarch-joypad-autoconfig/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="RetroArch joypad autoconfig files"
 PKG_TOOLCHAIN="manual"
@@ -17,5 +17,5 @@ makeinstall_target() {
 }
 
 post_makeinstall_target () {
-  cp -r $PKG_DIR/config/* ${INSTALL}/usr/share/retroarch/autoconfig
+  cp -av ${PKG_DIR}/config/* ${INSTALL}/usr/share/retroarch/autoconfig
 }
