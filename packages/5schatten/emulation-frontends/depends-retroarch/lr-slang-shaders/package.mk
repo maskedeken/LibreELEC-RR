40f3a659fd8307726f2be432e2e4071241155368
5schatten <supervisedthinking@gmail.com>
2019-05-07 23:49:37 +0200

    lr-slang-shaders: updated to git 932fe31

--- packages/5schatten/emulation-frontends/depends-retroarch/lr-slang-shaders/package.mk
+++ packages/5schatten/emulation-frontends/depends-retroarch/lr-slang-shaders/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
 
 PKG_NAME="lr-slang-shaders"
-PKG_VERSION="ade1215110a1f5ba1c83d224583548fab1f85a11"
-PKG_SHA256="cfd4358e01dbb37a4f4a7f7d3952c3211aafd2f6e740e952209a68897e2cc523"
+PKG_VERSION="932fe31f556794b610e6cfce77f23e77bdac5ba4"
+PKG_SHA256="3db84ccad99bab9343c66ac00e508ececfb6be33a6c61c8611e1e8e81b23f82e"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/slang-shaders"
 PKG_URL="https://github.com/libretro/slang-shaders/archive/${PKG_VERSION}.tar.gz"
