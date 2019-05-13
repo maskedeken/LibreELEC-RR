d8cd3d221f7f3010064b49fb43419b71930ce61f
5schatten <supervisedthinking@gmail.com>
2019-05-13 23:10:49 +0200

    lr-kronos: updated to git 23b6d99 / switched pkg repo / updated build

--- packages/5schatten/emulation-lr-cores/lr-kronos/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-kronos/package.mk
@@ -2,22 +2,55 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
 
 PKG_NAME="lr-kronos"
-PKG_VERSION="d626c039849283070b87215029412deb45fcdaea"
-PKG_SHA256="25a64db4a8c96cdebdf42d2a94c620ceeb8ec12810a2c3492c66b5c5f773af98"
-PKG_ARCH="x86_64"
+PKG_VERSION="23b6d9941480ea537c15784b72e839b96e78dfcb"
+PKG_SHA256="4d6e8cd5d795c4d09eba942a94dfcb942c30aa2cd74b21c85d2fbf7bda4bc514"
 PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/FCare/Kronos"
-PKG_URL="https://github.com/FCare/Kronos/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc mesa xorg-server"
+PKG_SITE="https://github.com/libretro/yabause"
+PKG_URL="https://github.com/libretro/yabause/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Kronos is a Sega Saturn emulator forked from uoYabause. "
 PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="kronos_libretro.so"
-PKG_LIBPATH="libretro/$PKG_LIBNAME"
+PKG_LIBPATH="yabause/src/libretro/${PKG_LIBNAME}"
 
-PKG_MAKE_OPTS_TARGET="-C libretro GIT_VERSION=${PKG_VERSION:0:7}"
+PKG_MAKE_OPTS_TARGET="-C yabause/src/libretro GIT_VERSION=${PKG_VERSION:0:7}"
+
+configure_package() {
+  # Apply project specific patches
+  PKG_PATCH_DIRS="${PROJECT}"
+
+  # Displayserver Support
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" xorg-server"
+  fi
+
+  # OpenGL Support
+  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  fi
+
+  # OpenGLES Support
+  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  fi
+}
+
+pre_configure_target() {
+  case ${PROJECT} in
+    Rockchip)
+      if [ "${DEVICE}" = "RK3399" ]; then
+        PKG_MAKE_OPTS_TARGET+=" platform=rockpro64"
+      fi
+    ;;
+  esac
+}
+
+pre_make_target() {
+  make CC=${HOST_CC} -C yabause/src/libretro generate-files
+}
 
 makeinstall_target() {
-    mkdir -p $INSTALL/usr/lib/libretro
-    cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
