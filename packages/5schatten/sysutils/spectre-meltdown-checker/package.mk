bba6cc8f326f2383c151c8402f746ce4bda76f2e
5schatten <supervisedthinking@gmail.com>
2019-05-15 14:56:09 +0200

    spectre-meltdown-checker: updated to v0.41

--- packages/5schatten/sysutils/spectre-meltdown-checker/package.mk
+++ packages/5schatten/sysutils/spectre-meltdown-checker/package.mk
@@ -2,17 +2,17 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
 
 PKG_NAME="spectre-meltdown-checker"
-PKG_VERSION="c705afe" # v0.40
-PKG_SHA256="d769b70f8c20dce6cb9f5fe62d3cbeceb5bef7ca420613d1e8cd540fc38d8531"
+PKG_VERSION="0.41"
+PKG_SHA256="979820b882bd4277cc6dc8c5b5a725a23c755ad001cfb413a113a780f2dc46e3"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/speed47/spectre-meltdown-checker"
-PKG_URL="https://github.com/speed47/spectre-meltdown-checker/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/speed47/spectre-meltdown-checker/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A shell script to tell if your system is vulnerable against the several speculative execution CVEs that were made public in 2018."
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
- mkdir -p $INSTALL/usr/bin
- chmod +x $PKG_BUILD/spectre-meltdown-checker.sh
- cp $PKG_BUILD/spectre-meltdown-checker.sh $INSTALL/usr/bin/sm-checker
+ mkdir -p ${INSTALL}/usr/bin
+ chmod +x ${PKG_BUILD}/spectre-meltdown-checker.sh
+ cp -v spectre-meltdown-checker.sh ${INSTALL}/usr/bin/spectre-meltdown-checker
 }
