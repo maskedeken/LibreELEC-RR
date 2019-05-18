d37cbfb70c2ad96c20f6c785045c4a19343fdf24
5schatten <supervisedthinking@gmail.com>
2019-05-18 05:18:32 +0200

    Skyscraper: updated email contact address in copyright header

--- packages/5schatten/sysutils/Skyscraper/package.mk
+++ packages/5schatten/sysutils/Skyscraper/package.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="Skyscraper"
 PKG_VERSION="3.1.4"
