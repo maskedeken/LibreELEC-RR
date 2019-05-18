710a2bb837a3a39910cce5a1c2bef0cee4d8cfbf
5schatten <supervisedthinking@gmail.com>
2019-05-18 02:57:50 +0200

    libevent: updated to v2.1.9-beta / updated pkg toolchain to CMake

--- packages/5schatten/depends/libevent/package.mk
+++ packages/5schatten/depends/libevent/package.mk
@@ -1,18 +1,19 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (at) gmail.com)
 
 PKG_NAME="libevent"
-PKG_VERSION="2.1.8-stable"
-PKG_SHA256="965cc5a8bb46ce4199a47e9b2c9e1cae3b137e8356ffdad6d94d3b9069b71dc2"
+PKG_VERSION="2.1.9-beta"
+PKG_SHA256="0ab250abac1def3d1e20e23e05ce827efa81db65c9004ccfff58d16404e3e369"
 PKG_LICENSE="BSD"
 PKG_SITE="http://libevent.org/"
-PKG_URL="https://github.com/libevent/libevent/releases/download/release-$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libevent/libevent/archive/release-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain openssl zlib"
-PKG_LONGDESC="The libevent API provides a mechanism to execute a callback function when a specific event occurs on a file descriptor or after a timeout has been reached. It is meant to replace the asynchronous event loop found in event-driven network servers."
+PKG_LONGDESC="Event notification library."
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-openssl \
-                           --disable-debug-mode"
+PKG_CMAKE_OPTS_TARGET="-DEVENT__DISABLE_DEBUG_MODE=on \
+                       -DEVENT__DISABLE_BENCHMARK=on \
+                       -DEVENT__DISABLE_TESTS=on \
+                       -DEVENT__DISABLE_SAMPLES=on"
 
 makeinstall_target() {
  :
