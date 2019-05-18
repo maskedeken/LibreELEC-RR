bcc39a957d2132510b24f7830a7a88d93ba1ca48
5schatten <supervisedthinking@gmail.com>
2019-05-18 05:17:13 +0200

    yquake2: updated email contact address in copyright header

--- packages/5schatten/emulation-standalone/yquake2/package.mk
+++ packages/5schatten/emulation-standalone/yquake2/package.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="yquake2"
 PKG_VERSION="a6b85ea" # v.7.3
