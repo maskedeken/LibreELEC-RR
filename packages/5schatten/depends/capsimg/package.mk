a35d9c86ba596de9e15f7c79ebde992cb0232c18
5schatten <supervisedthinking@gmail.com>
2019-05-18 04:59:56 +0200

    capsimg: updated email contact address in copyright header

--- packages/5schatten/depends/capsimg/package.mk
+++ packages/5schatten/depends/capsimg/package.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="capsimg"
 PKG_VERSION="067db4c"
