ec1022642e56157a664fc86de67ff1105da4a720
5schatten <supervisedthinking@gmail.com>
2019-05-18 04:21:55 +0200

    sdl-jstest: cleaned up

--- packages/5schatten/sysutils/sdl-jstest/package.mk
+++ packages/5schatten/sysutils/sdl-jstest/package.mk
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (at) gmail.com)
 
 PKG_NAME="sdl-jstest"
 PKG_VERSION="53b89cc2" #v0.2.1
@@ -8,15 +7,13 @@ PKG_LICENSE="GPLv2"
 PKG_SITE="https://gitlab.com/sdl-jstest/sdl-jstest"
 PKG_URL="https://gitlab.com/sdl-jstest/sdl-jstest.git"
 PKG_DEPENDS_TARGET="toolchain SDL2-git SDL_GameControllerDB ncurses"
-PKG_LONGDESC="sdl2-jstest are simple programs that lets you find out how many joysticks SDL2 detects on your system, how many axes, buttons, hats and balls they have each."
+PKG_LONGDESC="Simple SDL joystick test application for the console"
 GET_HANDLER_SUPPORT="git"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SDL_JSTEST=off"
 
 post_makeinstall_target() {
-  mv $INSTALL/usr/bin/sdl2-jstest $INSTALL/usr/bin/sdl2-jstest.bin
-  cp $PKG_DIR/scripts/* $INSTALL/usr/bin/
-  ln -sf /usr/bin/sdl2-jstest $INSTALL/usr/bin/sdl-jstest
+  mv ${INSTALL}/usr/bin/sdl2-jstest ${INSTALL}/usr/bin/sdl2-jstest.bin
+  cp ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin/
+  ln -sf /usr/bin/sdl2-jstest ${INSTALL}/usr/bin/sdl-jstest
 }
-
-
