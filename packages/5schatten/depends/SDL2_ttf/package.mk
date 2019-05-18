c590e7571d1dac1bf3735a69f74e1187b294deb0
5schatten <supervisedthinking@gmail.com>
2019-05-18 02:56:24 +0200

    SDL2_ttf: fixed freetype & patch handling

--- packages/5schatten/depends/SDL2_ttf/package.mk
+++ packages/5schatten/depends/SDL2_ttf/package.mk
@@ -1,18 +1,22 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (at) gmail.com)
 
 PKG_NAME="SDL2_ttf"
 PKG_VERSION="2.0.15"
 PKG_SHA256="a9eceb1ad88c1f1545cd7bd28e7cbc0b2c14191d40238f531a15b01b1b22cd33"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.libsdl.org/"
-PKG_URL="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-$PKG_VERSION.tar.gz"
+PKG_URL="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain SDL2-git freetype"
 PKG_LONGDESC="This is a sample library which allows you to use TrueType fonts in your SDL applications"
 PKG_TOOLCHAIN="configure"
 
-PKG_CONFIGURE_OPTS_TARGET="--with-freetype-prefix=$SYSROOT_PREFIX/usr"
+# Disable OpenGL if not supported
+if [ ! $OPENGL_SUPPORT = "yes" ]; then
+  PKG_PATCH_DIRS="no-opengl"
+fi
+
+PKG_CONFIGURE_OPTS_TARGET="--with-ft-prefix==${SYSROOT_PREFIX}/usr"
 
 if [ ! "${DISPLAYSERVER}" = "x11" ]; then
   PKG_CONFIGURE_OPTS_TARGET+=" --without-x"
