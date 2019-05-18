c2cb73c6448a0c70b6a206d470bfc79e657ad142
5schatten <supervisedthinking@gmail.com>
2019-05-18 19:12:08 +0200

    lr-fceumm: updated to git b4c8e40 / cleaned up a bit

--- packages/5schatten/emulation-lr-cores/lr-fceumm/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-fceumm/package.mk
@@ -2,21 +2,21 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-fceumm"
-PKG_VERSION="e4de631ecd6daad11a40fd1b8ba95a6634444a58"
-PKG_SHA256="1230df1a947c156d3d442a8e052da015ba9bba6b7668d0b3da6140ce76586f50"
+PKG_VERSION="b4c8e40ef1b624cab178eca8f17e802760cb8884"
+PKG_SHA256="e1fe6cc20a172d0f59e02cc4d19bd8c0cb3807b4db9bdbe56d7d2f54c0f1da06"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/libretro-fceumm"
-PKG_URL="https://github.com/libretro/libretro-fceumm/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/libretro-fceumm/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
-PKG_LONGDESC="FCEU mappers modified is an unofficial build of FCEU Ultra by CaH4e3, which supports a lot of new mappers including some obscure mappers such as one for unlicensed NES ROM's."
+PKG_LONGDESC="Libretro port of the NES/Faimcom emulator FCE Ultra mappers mod project."
 PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="fceumm_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="-f Makefile.libretro GIT_VERSION=${PKG_VERSION:0:7}"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
