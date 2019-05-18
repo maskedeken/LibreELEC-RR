ec795b7894163e8ad3f22c9566906ddddefd947a
5schatten <supervisedthinking@gmail.com>
2019-05-18 02:57:02 +0200

    SDL2_image: updated copyright header

--- packages/5schatten/depends/SDL2_image/package.mk
+++ packages/5schatten/depends/SDL2_image/package.mk
@@ -1,13 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (at) gmail.com)
 
 PKG_NAME="SDL2_image"
 PKG_VERSION="2.0.4"
 PKG_SHA256="e74ec49c2402eb242fbfa16f2f43a19582a74c2eabfbfb873f00d4250038ceac"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.libsdl.org/"
-PKG_URL="https://www.libsdl.org/projects/SDL_image/release/SDL2_image-$PKG_VERSION.tar.gz"
+PKG_URL="https://www.libsdl.org/projects/SDL_image/release/SDL2_image-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain SDL2-git"
-PKG_LONGDESC="SDL_image is an image file loading library. "
-
+PKG_LONGDESC="SDL_image is an image file loading library."
