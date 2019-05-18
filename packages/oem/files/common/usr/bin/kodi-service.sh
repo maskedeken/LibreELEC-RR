707de8ebdec78dd723e774a763d8e09a4c205c0f
5schatten <supervisedthinking@gmail.com>
2019-05-18 18:17:49 +0200

    oem/common: updated kodi-service.sh / changed function names for consistency

--- packages/oem/files/common/usr/bin/kodi-service.sh
+++ packages/oem/files/common/usr/bin/kodi-service.sh
@@ -19,19 +19,19 @@ kodi_cleanup_mute() {
   fi
 }
 
-kodi_mute_service() {
+kodi_service_mute() {
   kodi-send --action="RunScript(/usr/bin/kodi-service-mute.py)" > /dev/null
   echo "Muting Kodi service."
   touch ${RR_KODI_MUTE_STATE}
 }
 
-kodi_unmute_service() {
+kodi_service_unmute() {
   kodi-send --action="RunScript(/usr/bin/kodi-service-unmute.py)" > /dev/null
   echo "Unmuting Kodi service."
   kodi_cleanup_mute
 }
 
-kodi_start_service() {
+kodi_service_start() {
   kodi_cleanup_mute
   if [ ! "${RR_FLUIDSYNTH_SERVICE_STATE}" = "active" ]; then
     echo "Starting Kodi service."
@@ -52,7 +52,7 @@ kodi_start_service() {
   fi
 }
 
-kodi_stop_service() {
+kodi_service_stop() {
   kodi_cleanup_mute
   if [ "${1}" = "forceALSA" ]; then
     echo "Stopping Kodi service & force using ALSA backend."
@@ -77,28 +77,28 @@ kodi_stop_service() {
 case ${1} in
   --mute)
     if [ "${RR_KODI_SERVICE_STATE}" = "active" ] && [ ! -f "${RR_KODI_MUTE_STATE}" ]; then
-      kodi_mute_service
+      kodi_service_mute
     else
       echo "Kodi service was already muted or isn't running."
     fi
     ;;
   --unmute)
     if [ "${RR_KODI_SERVICE_STATE}" = "active" ] && [ -f "${RR_KODI_MUTE_STATE}" ]; then
-      kodi_unmute_service
+      kodi_service_unmute
     else
       echo "Kodi service was not muted or isn't running."
     fi
     ;;
   --start)
     if [ ! "${RR_KODI_SERVICE_STATE}" = "active" ]; then
-      kodi_start_service
+      kodi_service_start
     else
       echo "Kodi service is already running."
     fi
     ;;
   --stop)
     if [ "${RR_KODI_SERVICE_STATE}" = "active" ]; then
-      kodi_stop_service ${2}
+      kodi_service_stop ${2}
     else
       echo "Kodi service is already stopped."
     fi
@@ -107,8 +107,8 @@ case ${1} in
     echo "Usage:" 
     echo -e "  ${0} --[mute|unmute|start|stop] [forceALSA]\n"
     echo "Kodi service options:"
-    echo "  --mute           - mutes   audio of Kodi service"
-    echo "  --unmute         - unmutes audio of Kodi service"
+    echo "  --mute           - mutes   Kodi audio & stopps video player"
+    echo "  --unmute         - unmutes Kodi audio"
     echo "  --start          - starts  Kodi service"
     echo "  --stop           - stops   Kodi service & starts audio services depending on configured backend"
     echo "  --stop forceALSA - stops   Kodi service & forces ALSA audio"
