f27b216dc6cb71ceb28a352dccff5d6e48ce6c3c
5schatten <supervisedthinking@gmail.com>
2019-05-18 02:57:12 +0200

    makemkv-bin: updated copyright header

--- packages/5schatten/depends/makemkv-bin/package.mk
+++ packages/5schatten/depends/makemkv-bin/package.mk
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) Originally created by Escalade (https://github.com/escalade)
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (at) gmail.com)
 
 PKG_NAME="makemkv-bin"
 PKG_VERSION="1.14.3"
@@ -8,7 +7,7 @@ PKG_SHA256="e0b10e94ea98643e05da009f867e58481e721fb5c0551d6e5aa2f7756f3e2bb4"
 PKG_ARCH="x86_64"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.makemkv.com/"
-PKG_URL="http://www.makemkv.com/download/makemkv-bin-$PKG_VERSION.tar.gz"
+PKG_URL="http://www.makemkv.com/download/makemkv-bin-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="MakeMKV binaries & EULA"
 PKG_TOOLCHAIN="manual"
