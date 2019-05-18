8cad19be37e4b0472fc0205bca684523758475e8
5schatten <supervisedthinking@gmail.com>
2019-05-18 05:18:09 +0200

    midnight-commander: updated email contact address in copyright header

--- packages/5schatten/sysutils/midnight-commander/package.mk
+++ packages/5schatten/sysutils/midnight-commander/package.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="midnight-commander"
 PKG_VERSION="4.8.22"
