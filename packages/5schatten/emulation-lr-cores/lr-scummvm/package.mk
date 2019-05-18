8d18ac589a1bb4781fc09e5ea8b7a9c2754d8f98
5schatten <supervisedthinking@gmail.com>
2019-05-18 05:15:01 +0200

    lr-scummvm: updated email contact address in copyright header

--- packages/5schatten/emulation-lr-cores/lr-scummvm/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-scummvm/package.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-scummvm"
 PKG_VERSION="602a26cedd55677942648cbf488f52bd951a95d8"
