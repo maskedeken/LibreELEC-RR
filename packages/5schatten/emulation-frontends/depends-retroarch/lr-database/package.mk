eb31b9e974ef89796738f9e0363a2c5316d6d4b8
5schatten <supervisedthinking@gmail.com>
2019-05-18 05:06:00 +0200

    lr-database: updated email contact address in copyright header

--- packages/5schatten/emulation-frontends/depends-retroarch/lr-database/package.mk
+++ packages/5schatten/emulation-frontends/depends-retroarch/lr-database/package.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-database"
 PKG_VERSION="d491e37bb9bb2b9126e70ac56b7ca38482ab4435"
