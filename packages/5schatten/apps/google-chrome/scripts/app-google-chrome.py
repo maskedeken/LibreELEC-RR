6c91fd222c524073179622516f17bd97a452fdd3
5schatten <supervisedthinking@gmail.com>
2019-05-18 17:00:26 +0200

    google-chrome: updated script name scheme

--- packages/5schatten/apps/google-chrome/scripts/app-google-chrome.py
+++ packages/5schatten/apps/google-chrome/scripts/app-google-chrome.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 import subprocess
-subprocess.call("systemd-run /usr/bin/google-chrome-stable", shell=True)
+subprocess.call("systemd-run /usr/bin/google-chrome.start", shell=True)
