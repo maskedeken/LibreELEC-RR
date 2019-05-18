9e6fd18caec99adcda611eaf0ca2a075e3c88fd8
5schatten <supervisedthinking@gmail.com>
2019-05-18 04:35:59 +0200

    emulationstation: updated copyright headers

--- packages/5schatten/emulation-frontends/emulationstation/package.mk
+++ packages/5schatten/emulation-frontends/emulationstation/package.mk
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (at) gmail.com)
 
 PKG_NAME="emulationstation"
 PKG_VERSION="3a9297abbb84429138be4b4ee0f1e4c1d7726475" #v2.8.3
