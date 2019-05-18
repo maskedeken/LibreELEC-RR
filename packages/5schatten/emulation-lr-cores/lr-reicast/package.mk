d309271b3bf1cdd36c097e61c691169628477b98
5schatten <supervisedthinking@gmail.com>
2019-05-18 05:14:50 +0200

    lr-reicast: updated email contact address in copyright header

--- packages/5schatten/emulation-lr-cores/lr-reicast/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-reicast/package.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-reicast"
 PKG_VERSION="2e3813fc7a48e7c192496a08d6223d117410aa7f"
