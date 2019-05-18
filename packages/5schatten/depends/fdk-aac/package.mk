1ddd112fb421ea5cec5e87e68f5fd4b4bff15e7d
5schatten <supervisedthinking@gmail.com>
2019-05-18 05:00:19 +0200

    fdk-aac: updated email contact address in copyright header

--- packages/5schatten/depends/fdk-aac/package.mk
+++ packages/5schatten/depends/fdk-aac/package.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2019-present Frank Hartung (supervisedthinking@gmail.com)
+# Copyright (C) 2019-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="fdk-aac"
 PKG_VERSION="2.0.0"
