eaab9c57c4499dd6d43d99272a60c6247126228c
5schatten <supervisedthinking@gmail.com>
2019-05-19 21:27:47 +0200

    lr-nestopia: updated to git 7e288bb

--- packages/5schatten/emulation-lr-cores/lr-nestopia/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-nestopia/package.mk
@@ -2,17 +2,17 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-nestopia"
-PKG_VERSION="44800c5f6336894a8307cc03a38403dbfda83d29"
-PKG_SHA256="ae51ae7731aa1fdcd684b2497148d18250c6b0b043bc36319d9d540db8c24cb7"
+PKG_VERSION="7e288bbf9b989192f57d8973c7d76336f731be61"
+PKG_SHA256="4dcb2e280e6243f46e52d26396b27c504a5515c9477ce844a21eb18fb10616f0"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/nestopia"
-PKG_URL="https://github.com/libretro/nestopia/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/nestopia/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
-PKG_LONGDESC="This project is a fork of the original Nestopia source code, plus the Linux port. The purpose of the project is to enhance the original, and ensure it continues to work on modern operating systems."
+PKG_LONGDESC="This project is a fork of the original Nestopia source code, plus the Linux port"
 PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="nestopia_libretro.so"
-PKG_LIBPATH="libretro/$PKG_LIBNAME"
+PKG_LIBPATH="libretro/${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="-C libretro GIT_VERSION=${PKG_VERSION:0:7}"
 
@@ -23,6 +23,6 @@ pre_configure_target() {
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
