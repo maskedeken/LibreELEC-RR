848d3197ac82ba24cd686844ad5cde78bb7b7b51
5schatten <supervisedthinking@gmail.com>
2019-05-19 21:28:24 +0200

    lr-prboom: updated to git 43716f7 / fixed license

--- packages/5schatten/emulation-lr-cores/lr-prboom/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-prboom/package.mk
@@ -2,28 +2,27 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-prboom"
-PKG_VERSION="5371c885f195b60448207f2a8f27861a34a0d287"
-PKG_SHA256="18ac82a2b3377d86696f9169f18c0644d0a4c60aa17b8e2eae0782ec81fef6f4"
-PKG_LICENSE="GPLv3"
+PKG_VERSION="43716f784506aeeef80f054685c471f1508a46f9"
+PKG_SHA256="566375956eec705ebef5803cf07e3f15c4da7f1666fd4093943afc85a1d8f4ae"
+PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/libretro-prboom"
-PKG_URL="https://github.com/libretro/libretro-prboom/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/libretro-prboom/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Port of prboom to libretro - plays Doom, Doom II, Final Doom and other Doom IWAD mods."
 PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="prboom_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/bin
-  mkdir -p $INSTALL/usr/lib/libretro
-  mkdir -p $INSTALL/usr/share/prboom
+  mkdir -p ${INSTALL}/usr/bin
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  mkdir -p ${INSTALL}/usr/share/prboom
 
-  cp $PKG_DIR/scripts/* $INSTALL/usr/bin
-  cp $PKG_BUILD/prboom.wad $INSTALL/usr/share/prboom
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  cp ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin
+  cp ${PKG_BUILD}/prboom.wad ${INSTALL}/usr/share/prboom
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
-
 
