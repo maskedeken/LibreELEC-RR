5dd25881c1d698f02459e52ee2057096ea47bb28
5schatten <supervisedthinking@gmail.com>
2019-05-19 13:55:58 +0200

    PCSX2: clean up & improved build / switched repo

--- packages/5schatten/emulation-standalone/PCSX2/package.mk
+++ packages/5schatten/emulation-standalone/PCSX2/package.mk
@@ -3,20 +3,23 @@
 
 PKG_NAME="PCSX2"
 PKG_VERSION="1.5.0-dev"
-PKG_DOCKER_IMAGE_VERSION="1.0.2"
+PKG_DOCKER_IMAGE_VERSION="1.0.0"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/PCSX2/pcsx2"
-PKG_URL=""
 PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="PCSX2 is a free and open-source PlayStation 2 (PS2) emulator."
+PKG_LONGDESC="PCSX2 is a free and open-source PS2 emulator."
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
- mkdir -p $INSTALL/usr/bin
- mkdir -p $INSTALL/usr/config
+ # Create directories
+ mkdir -p ${INSTALL}/usr/bin
+ mkdir -p ${INSTALL}/usr/config
 
- cp -r $PKG_DIR/scripts/* $INSTALL/usr/bin/
- cp -r $PKG_DIR/config/* $INSTALL/usr/config/
- sed -e "s/PCSX2_DOCKER_IMAGE_VERSION=.*/PCSX2_DOCKER_IMAGE_VERSION=version-${PKG_DOCKER_IMAGE_VERSION}/" -i ${INSTALL}/usr/bin/pcsx2.start
+ # Install scripts & config files
+ cp -rfv ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin/
+ cp -rfv ${PKG_DIR}/config/*  ${INSTALL}/usr/config/
+
+ # Update Docker image version
+ sed -e "s/PCSX2_DOCKER_IMAGE_VERSION=.*/PCSX2_DOCKER_IMAGE_VERSION=v${PKG_DOCKER_IMAGE_VERSION}/" -i ${INSTALL}/usr/bin/pcsx2.start
 }
