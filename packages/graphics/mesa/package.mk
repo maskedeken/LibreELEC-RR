60e05fe557ff010742c26b143d44bcb1b1c820af
5schatten <supervisedthinking@gmail.com>
2019-05-21 16:38:54 +0200

    mesa: update to mesa-19.1.0-rc3

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="19.1.0-rc2"
-PKG_SHA256="d0fb89935d347fa15dc00a264bb888931d84a33c947dd8c0d7bcc0106e372f47"
+PKG_VERSION="19.1.0-rc3"
+PKG_SHA256="0d3000163a6074e1867bf33cd929b72cd68a4a4fbe57455c92eda9706591150d"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://github.com/mesa3d/mesa/archive/$PKG_NAME-$PKG_VERSION.tar.gz"
