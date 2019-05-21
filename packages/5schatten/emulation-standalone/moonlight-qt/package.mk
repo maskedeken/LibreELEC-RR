d3dafe23b664d335c2d0ba1bce2ecdcbac1158d2
5schatten <supervisedthinking@gmail.com>
2019-05-22 00:53:52 +0200

    moonlight-qt: initial package v1.0.0 / git 3df9a5d

---
+++ packages/5schatten/emulation-standalone/moonlight-qt/package.mk
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="moonlight-qt"
+PKG_VERSION="3df9a5d01c434f38cf1963972e394290e4b8b4c8" # v1.0.0
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/moonlight-stream/moonlight-qt"
+PKG_URL="https://github.com/moonlight-stream/moonlight-qt.git"
+PKG_DEPENDS_TARGET="toolchain linux openssl alsa-lib pulseaudio ffmpeg SDL2-git SDL2_ttf qt-everywhere"
+PKG_LONGDESC="Moonlight is an open source implementation of NVIDIA's GameStream."
+GET_HANDLER_SUPPORT="git"
+PKG_TOOLCHAIN="make"
+
+configure_package() {
+  # Displayserver Support
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" xorg-server unclutter-xfixes"
+  fi
+}
+
+configure_target() {
+  # Create working dir
+  mkdir -p ${PKG_BUILD}/.${TARGET_NAME}
+  cd .${TARGET_NAME}
+
+  # Generate qmake config
+  qmake ${PKG_BUILD}/moonlight-qt.pro PREFIX=${INSTALL}/usr
+}
+
+post_makeinstall_target() {
+  mv ${INSTALL}/usr/bin/moonlight ${INSTALL}/usr/bin/moonlight-qt
+  cp -rfv ${PKG_DIR}/scripts/*    ${INSTALL}/usr/bin/
+  safe_remove ${INSTALL}/usr/share
+}
