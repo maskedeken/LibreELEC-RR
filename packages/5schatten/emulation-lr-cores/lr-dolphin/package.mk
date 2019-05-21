aa082d564d5289654cb3547e1c3db66dc4435ec2
5schatten <supervisedthinking@gmail.com>
2019-05-21 16:33:15 +0200

    lr-dolphin: updated to 68f9fa7

--- packages/5schatten/emulation-lr-cores/lr-dolphin/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-dolphin/package.mk
@@ -2,17 +2,17 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-dolphin"
-PKG_VERSION="1d973f4170e18f87b863d6534c077ac7b6fc7ce2"
-PKG_SHA256="18864071af1cd3bc434a01cd6ec43036dd7ef4ad1f26edc20baba5aa46dd319d"
+PKG_VERSION="68f9fa70b461a4ce4dcb1b19d72799e4661a952a"
+PKG_SHA256="88af2b8c1ac4e290a0ca0fc4fd8b976da29867dafedfcb589a660897161e75b0"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/dolphin"
-PKG_URL="https://github.com/libretro/dolphin/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/dolphin/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc systemd mesa xorg-server enet bluez lzo alsa-lib ffmpeg curl libpng zlib"
 PKG_LONGDESC="Dolphin is a GameCube / Wii emulator, allowing you to play games for these two platforms on PC with improvements."
 
 PKG_LIBNAME="dolphin_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 
 pre_configure_target() {
   PKG_CMAKE_OPTS_TARGET="-DENABLE_QT=OFF \
@@ -23,11 +23,11 @@ pre_configure_target() {
 
 pre_make_target() {
   # fix cross compiling
-  find $PKG_BUILD -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
-  find $PKG_BUILD -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
