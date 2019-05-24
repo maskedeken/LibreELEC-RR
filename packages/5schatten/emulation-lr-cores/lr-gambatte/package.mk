36812d4fecc0ac48b2d71de2dd2cfb411a4e2eb3
5schatten <supervisedthinking@gmail.com>
2019-05-24 15:16:40 +0200

    lr-gambatte: updated to git a67455e

--- packages/5schatten/emulation-lr-cores/lr-gambatte/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-gambatte/package.mk
@@ -2,18 +2,18 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-gambatte"
-PKG_VERSION="7d4beec1ff69098a602c12d38a76465510cb8ebc"
-PKG_SHA256="ed8b68ac1d7378366818a5887afcf8cf83f1d00ed572852becd9e78b62baa90c"
+PKG_VERSION="a67455ea3f4245b569d2b3c8ca23fe0fe24377c2"
+PKG_SHA256="7493d6d9e12056a6fd3b38fabf7aacd0d4392e1d432c512418269189404b2731"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/gambatte-libretro"
-PKG_URL="https://github.com/libretro/gambatte-libretro/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/gambatte-libretro/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Gambatte is an accuracy-focused, open-source, cross-platform Game Boy Color emulator written in C++."
 
 PKG_LIBNAME="gambatte_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 
 makeinstall_target() {
-    mkdir -p $INSTALL/usr/lib/libretro
-    cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+    mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
