532410a996910af2e8a5e9293052aaf5dc309de1
5schatten <supervisedthinking@gmail.com>
2019-05-24 15:14:24 +0200

    lr-common-overlays: updated to git fc8e10c

--- packages/5schatten/emulation-frontends/depends-retroarch/lr-common-overlays/package.mk
+++ packages/5schatten/emulation-frontends/depends-retroarch/lr-common-overlays/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-common-overlays"
-PKG_VERSION="3e9102a717f3e294867d71c674dcebece0bb6cb2"
-PKG_SHA256="619d0900a915cf5c27f600ada7b52066247be2f728a3fba0537e4a57dad24a48"
+PKG_VERSION="fc8e10cce56fbd771600ead36737e85b93d606ed"
+PKG_SHA256="551b37a413b067d431b0fbf5d80e324a32b9c619091d5c596d1346fe1c3179d2"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/common-overlays"
 PKG_URL="https://github.com/libretro/common-overlays/archive/$PKG_VERSION.tar.gz"
