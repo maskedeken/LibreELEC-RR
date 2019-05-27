b3886fdffbe84ca5cf6d715cfd6b894fc3ce3e5a
5schatten <supervisedthinking@gmail.com>
2019-05-27 14:34:43 +0200

    lr-desmume: added lto build flag

--- packages/5schatten/emulation-lr-cores/lr-desmume/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-desmume/package.mk
@@ -6,16 +6,17 @@ PKG_VERSION="b9b515f57bfd5a6ca73590b6d3de4813789b2a3a"
 PKG_SHA256="02a7fd65d30147842cd3c26f6304c2efebda0de513761f849d10c1fa63fe9319"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/desmume"
-PKG_URL="https://github.com/libretro/desmume/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/desmume/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc libpcap"
 PKG_LONGDESC="DeSmuME is a Nintendo DS emulator"
 PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
 
 PKG_LIBNAME="desmume_libretro.so"
-PKG_LIBPATH="desmume/src/frontend/libretro/$PKG_LIBNAME"
+PKG_LIBPATH="desmume/src/frontend/libretro/${PKG_LIBNAME}"
 
 # Disable OpenGL if not supported
-if [ ! $OPENGL_SUPPORT = "yes" ]; then
+if [ ! ${OPENGL_SUPPORT} = "yes" ]; then
   PKG_PATCH_DIRS="no-opengl"
 fi
 
@@ -34,17 +35,17 @@ configure_package() {
 }
 
 pre_configure_target() {
-  case $TARGET_CPU in
+  case ${TARGET_CPU} in
     arm1176jzf-s)
-      PKG_MAKE_OPTS_TARGET+=" platform=armv6-hardfloat-$TARGET_CPU"
+      PKG_MAKE_OPTS_TARGET+=" platform=armv6-hardfloat-${TARGET_CPU}"
       ;;
     cortex-a7|cortex-a9|*cortex-a53)
-      PKG_MAKE_OPTS_TARGET+=" platform=armv7-neon-hardfloat-$TARGET_CPU"
+      PKG_MAKE_OPTS_TARGET+=" platform=armv7-neon-hardfloat-${TARGET_CPU}"
       ;;
   esac
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
