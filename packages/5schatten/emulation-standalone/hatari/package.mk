186f6c084986056e744e3ef690cf0a14243fa78a
5schatten <supervisedthinking@gmail.com>
2019-05-27 19:57:28 +0200

    hatari: updated to git 50bf142 / cleaned up a bit

--- packages/5schatten/emulation-standalone/hatari/package.mk
+++ packages/5schatten/emulation-standalone/hatari/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="hatari"
-PKG_VERSION="7aca27d0bb3c228b2cdbb92e0403df2c08ac9398" #v2.2.1+
-PKG_SHA256="abcbe1a2046ca90b085b3fbd8dba689e5637a52832283caadba51ce4123da378"
+PKG_VERSION="50bf142e68841f6b708911ff6cb2f52452c5769c" #v2.2.1+
+PKG_SHA256="061f99f14c002ffb05c016ff9e52a7d145f764d41782f9e64d6c7b242e75f962"
 PKG_LICENSE="GPL"
 PKG_SITE="https://hatari.tuxfamily.org/"
-PKG_URL="https://github.com/hatari/hatari/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/hatari/hatari/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc systemd alsa-lib SDL2-git portaudio zlib capsimg libpng"
 PKG_LONGDESC="Hatari is an Atari ST/STE/TT/Falcon emulator"
 PKG_BUILD_FLAGS="+lto"
@@ -22,29 +22,29 @@ pre_configure_target() {
   PKG_CMAKE_OPTS_TARGET="-DCMAKE_SKIP_RPATH=ON \
                          -DDATADIR="/usr/config/hatari" \
                          -DBIN2DATADIR="../../storage/.config/hatari" \
-                         -DCAPSIMAGE_INCLUDE_DIR=$PKG_BUILD/src/include \
-                         -DCAPSIMAGE_LIBRARY=$PKG_BUILD/libcapsimage.so.5.1"
+                         -DCAPSIMAGE_INCLUDE_DIR=${PKG_BUILD}/src/include \
+                         -DCAPSIMAGE_LIBRARY=${PKG_BUILD}/libcapsimage.so.5.1"
 
   # copy IPF Support Library include files
-  mkdir -p $PKG_BUILD/src/includes/caps5/
-  cp -R $(get_build_dir capsimg)/LibIPF/* $PKG_BUILD/src/includes/caps5/
-  cp -R $(get_build_dir capsimg)/Core/CommonTypes.h $PKG_BUILD/src/includes/caps5/
-  cp -R $(get_build_dir capsimg)/libcapsimage.so.5.1 $PKG_BUILD/
+  mkdir -p ${PKG_BUILD}/src/includes/caps5/
+  cp -R $(get_build_dir capsimg)/LibIPF/* ${PKG_BUILD}/src/includes/caps5/
+  cp -R $(get_build_dir capsimg)/Core/CommonTypes.h ${PKG_BUILD}/src/includes/caps5/
+  cp -R $(get_build_dir capsimg)/libcapsimage.so.5.1 ${PKG_BUILD}/
 
   # add library search path for loading libcapsimage library
-  LDFLAGS="$LDFLAGS -Wl,-rpath='$PKG_BUILD'"
+  LDFLAGS+=" -Wl,-rpath='${PKG_BUILD}'"
 }
 
 makeinstall_target() {
   # create directories
-  mkdir -p $INSTALL/usr/bin
-  mkdir -p $INSTALL/usr/config/hatari
+  mkdir -p ${INSTALL}/usr/bin
+  mkdir -p ${INSTALL}/usr/config/hatari
 
   # copy config files  
-  touch $INSTALL/usr/config/hatari/hatari.nvram
-  cp -R $PKG_DIR/config/* $INSTALL/usr/config/hatari
+  touch ${INSTALL}/usr/config/hatari/hatari.nvram
+  cp -R ${PKG_DIR}/config/* ${INSTALL}/usr/config/hatari
 
   # copy binary & start script
-  cp src/hatari $INSTALL/usr/bin
-  cp -R $PKG_DIR/scripts/* $INSTALL/usr/bin/
+  cp src/hatari ${INSTALL}/usr/bin
+  cp -R ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin/
 }
