5fbbc6dbda9ff74e59f7c1b29617f7157efdda51
5schatten <supervisedthinking@gmail.com>
2019-05-27 14:38:13 +0200

    lr-mesen: added lto build flag

--- packages/5schatten/emulation-lr-cores/lr-mesen/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-mesen/package.mk
@@ -11,6 +11,7 @@ PKG_URL="https://github.com/SourMesen/Mesen/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Mesen is a cross-platform (Windows & Linux) NES/Famicom emulator built in C++ and C#"
 PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
 
 PKG_LIBNAME="mesen_libretro.so"
 PKG_LIBPATH="Libretro/${PKG_LIBNAME}"
@@ -19,5 +20,5 @@ PKG_MAKE_OPTS_TARGET="-C Libretro"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-  cp ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
