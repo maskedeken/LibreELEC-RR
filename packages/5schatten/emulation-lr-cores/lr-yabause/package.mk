383c5b5ab1521dfe02d9b4af5b6a127a2779deaa
5schatten <supervisedthinking@gmail.com>
2019-05-27 14:40:26 +0200

    lr-yabause: added lto build flag

--- packages/5schatten/emulation-lr-cores/lr-yabause/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-yabause/package.mk
@@ -6,17 +6,18 @@ PKG_VERSION="aa15301b1d1b49d035d0672a1ccaa5631211b29b"
 PKG_SHA256="268e356c6c3f30a88f8cd3394a48d863d3166cbc060fe7e7eb02be4a93a38f7c"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/yabause"
-PKG_URL="https://github.com/libretro/yabause/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/yabause/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Yabause is a Sega Saturn emulator."
 PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
 
 PKG_LIBNAME="yabause_libretro.so"
-PKG_LIBPATH="libretro/$PKG_LIBNAME"
+PKG_LIBPATH="libretro/${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="-C libretro GIT_VERSION=${PKG_VERSION:0:7}"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
