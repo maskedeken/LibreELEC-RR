055ddb124bc445ed09fd6287bd60b27afda4a409
5schatten <supervisedthinking@gmail.com>
2019-05-27 14:37:45 +0200

    lr-kronos: added lto build flag

--- packages/5schatten/emulation-lr-cores/lr-kronos/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-kronos/package.mk
@@ -10,6 +10,7 @@ PKG_URL="https://github.com/libretro/yabause/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Kronos is a Sega Saturn emulator forked from uoYabause. "
 PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
 
 PKG_LIBNAME="kronos_libretro.so"
 PKG_LIBPATH="yabause/src/libretro/${PKG_LIBNAME}"
