56184219e8bc6f5191b49cdb2161d14ad09ff37d
5schatten <supervisedthinking@gmail.com>
2019-05-27 14:39:34 +0200

    lr-snes9x: added lto build flag

--- packages/5schatten/emulation-lr-cores/lr-snes9x/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-snes9x/package.mk
@@ -6,17 +6,18 @@ PKG_VERSION="e5ee711624a351c266fe2394961c469a5b708a8c"
 PKG_SHA256="ef27f892fb0179e65d69457dabb5a9e18fad5599e4eac6937897ece74c2e1757"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/snes9x"
-PKG_URL="https://github.com/libretro/snes9x/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/snes9x/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Snes9x - Portable Super Nintendo Entertainment System (TM) emulator"
 PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
 
 PKG_LIBNAME="snes9x_libretro.so"
-PKG_LIBPATH="libretro/$PKG_LIBNAME"
+PKG_LIBPATH="libretro/${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="-C libretro GIT_VERSION=${PKG_VERSION:0:7}"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
