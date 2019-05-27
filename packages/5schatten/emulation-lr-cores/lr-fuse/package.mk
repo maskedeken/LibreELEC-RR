9f6d3c5cd028de6b0b9d223e3a2a2d260656d8fa
5schatten <supervisedthinking@gmail.com>
2019-05-27 14:37:23 +0200

    lr-fuse: added lto build flag

--- packages/5schatten/emulation-lr-cores/lr-fuse/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-fuse/package.mk
@@ -6,14 +6,15 @@ PKG_VERSION="98b9ba17bad15a6ed60831f53ba34654b1a1bcde"
 PKG_SHA256="43552fc1ca6abee93d53bb616519815a4494339aa1dbc989c732523a8faebcf7"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/fuse-libretro"
-PKG_URL="https://github.com/libretro/fuse-libretro/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/fuse-libretro/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="fuse-libretro is an work in progress port of the Fuse Unix Spectrum Emulator to libretro."
+PKG_BUILD_FLAGS="+lto"
 
 PKG_LIBNAME="fuse_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
