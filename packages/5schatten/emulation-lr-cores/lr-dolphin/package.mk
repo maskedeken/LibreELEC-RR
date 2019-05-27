9131a49fe8e4cd89e61f5d16527a9ce470fedd4f
5schatten <supervisedthinking@gmail.com>
2019-05-27 14:37:02 +0200

    lr-dolphin: added lto build flag

--- packages/5schatten/emulation-lr-cores/lr-dolphin/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-dolphin/package.mk
@@ -10,6 +10,7 @@ PKG_SITE="https://github.com/libretro/dolphin"
 PKG_URL="https://github.com/libretro/dolphin/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc systemd mesa xorg-server enet bluez lzo alsa-lib ffmpeg curl libpng zlib"
 PKG_LONGDESC="Dolphin is a GameCube / Wii emulator, allowing you to play games for these two platforms on PC with improvements."
+PKG_BUILD_FLAGS="+lto"
 
 PKG_LIBNAME="dolphin_libretro.so"
 PKG_LIBPATH="${PKG_LIBNAME}"
