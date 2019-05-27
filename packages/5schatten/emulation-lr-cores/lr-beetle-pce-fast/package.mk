c5f59eee8dc0cb90796c85fe625226b4a4f32148
5schatten <supervisedthinking@gmail.com>
2019-05-27 14:33:04 +0200

    lr-beetle-pce-fast: added lto build flag

--- packages/5schatten/emulation-lr-cores/lr-beetle-pce-fast/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-beetle-pce-fast/package.mk
@@ -10,6 +10,7 @@ PKG_URL="https://github.com/libretro/beetle-pce-fast-libretro/archive/$PKG_VERSI
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Standalone port of Mednafen PCE Fast to libretro."
 PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
 
 PKG_LIBNAME="mednafen_pce_fast_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
