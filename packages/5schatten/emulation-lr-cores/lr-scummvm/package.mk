d35cbc28752ad1aa09e97822578c443621704ec5
5schatten <supervisedthinking@gmail.com>
2019-05-27 14:39:26 +0200

    lr-scummvm: clean up

--- packages/5schatten/emulation-lr-cores/lr-scummvm/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-scummvm/package.mk
@@ -6,24 +6,24 @@ PKG_VERSION="602a26cedd55677942648cbf488f52bd951a95d8"
 PKG_SHA256="683d1fc76ccc78603c02a5c51bc6f633bae33d6f0ce1a4ffd8d4753de47f1edf"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/scummvm"
-PKG_URL="https://github.com/libretro/scummvm/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/scummvm/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="ScummVM is an interpreter for point-and-click adventure games that can be used as a libretro core."
 PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="scummvm_libretro.so"
-PKG_LIBPATH="backends/platform/libretro/build/$PKG_LIBNAME"
+PKG_LIBPATH="backends/platform/libretro/build/${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="-C backends/platform/libretro/build/ GIT_VERSION=${PKG_VERSION:0:7}"
 
 pre_configure_target() {
-  cd $PKG_BUILD
+  cd ${PKG_BUILD}
   CXXFLAGS+=" -DHAVE_POSIX_MEMALIGN=1"
-  export LD="$CC"
+  export LD="${CC}"
   export AR+=" cru"
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
