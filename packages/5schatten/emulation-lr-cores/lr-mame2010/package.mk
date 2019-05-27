c64a6c0b432400924806a04e228b4bd9d41e3151
5schatten <supervisedthinking@gmail.com>
2019-05-27 14:38:01 +0200

    lr-mame2010: added lto build flag

--- packages/5schatten/emulation-lr-cores/lr-mame2010/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-mame2010/package.mk
@@ -6,27 +6,28 @@ PKG_VERSION="c87309dab5fc8db292e31737fcd1754de1fbae6a"
 PKG_SHA256="6f442b362b5929f8b762f289a874e2011902617efdcea4bfc2fc2a54836973ec"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame2010-libretro"
-PKG_URL="https://github.com/libretro/mame2010-libretro/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/mame2010-libretro/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc zlib"
 PKG_LONGDESC="Late 2010 version of MAME (0.139) for libretro. Compatible with MAME 0.139 romsets."
 PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
 
 PKG_LIBNAME="mame2010_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
 
 pre_configure_target() {
-  export CFLAGS="$CFLAGS -fpermissive"
-  export CXXFLAGS="$CXXFLAGS -fpermissive"
-  export LD="$CXX"
+  export CFLAGS+=" -fpermissive"
+  export CXXFLAGS+=" -fpermissive"
+  export LD="${CXX}"
 
-  case $TARGET_CPU in
+  case ${TARGET_CPU} in
     arm1176jzf-s)
-      PKG_MAKE_OPTS_TARGET+=" platform=armv6-hardfloat-$TARGET_CPU"
+      PKG_MAKE_OPTS_TARGET+=" platform=armv6-hardfloat-${TARGET_CPU}"
       ;;
     cortex-a7|cortex-a9)
-      PKG_MAKE_OPTS_TARGET+=" platform=armv7-neon-hardfloat-$TARGET_CPU"
+      PKG_MAKE_OPTS_TARGET+=" platform=armv7-neon-hardfloat-${TARGET_CPU}"
       ;;
     *cortex-a53|cortex-a17)
       PKG_MAKE_OPTS_TARGET+=" platform=armv7-neon-hardfloat-cortex-a9"
@@ -35,6 +36,6 @@ pre_configure_target() {
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
