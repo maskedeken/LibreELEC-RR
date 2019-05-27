9989d6760df2d89eab543fa849d9565ff75137ea
5schatten <supervisedthinking@gmail.com>
2019-05-27 17:52:16 +0200

    SDL_GameControllerDB: updated to git d57f564 / improved build & cleaned up

--- packages/5schatten/depends/SDL_GameControllerDB/package.mk
+++ packages/5schatten/depends/SDL_GameControllerDB/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="SDL_GameControllerDB"
-PKG_VERSION="cbebd79740bc1360978bdf6d9d4d13d189e420a3"
-PKG_SHA256="a1274ed0acf6cf4c8596d1a55a240db686d86ee346d896fdb8bc8c10c2a27415"
+PKG_VERSION="d57f56464971268ca0811b6ead1279b73f173a77"
+PKG_SHA256="df28c216bb3fa4b80f46eeba61254621bf1879d69ce966acd6b2fa4c47d2c928"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/gabomdq/SDL_GameControllerDB"
 PKG_URL="https://github.com/gabomdq/SDL_GameControllerDB/archive/${PKG_VERSION}.tar.gz"
@@ -12,6 +12,13 @@ PKG_LONGDESC="A community sourced database of game controller mappings to be use
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  mkdir -p ${INSTALL}/usr/config/SDL-GameControllerDB
-  cp ${PKG_BUILD}/gamecontrollerdb.txt ${INSTALL}/usr/config/SDL-GameControllerDB
+  PKG_INSTALL_PATH=${INSTALL}/usr/config/SDL-GameControllerDB
+  mkdir -p ${PKG_INSTALL_PATH}
+  cp -v gamecontrollerdb.txt ${PKG_INSTALL_PATH}
+  # Clean up
+  sed -e "/Windows/d" -i ${PKG_INSTALL_PATH}/gamecontrollerdb.txt
+  sed -e "/Mac/d"     -i ${PKG_INSTALL_PATH}/gamecontrollerdb.txt
+  sed -e "/Android/d" -i ${PKG_INSTALL_PATH}/gamecontrollerdb.txt
+  sed -e "/iOS/d"     -i ${PKG_INSTALL_PATH}/gamecontrollerdb.txt
+  sed -e "/^$/d"      -i ${PKG_INSTALL_PATH}/gamecontrollerdb.txt
 }
