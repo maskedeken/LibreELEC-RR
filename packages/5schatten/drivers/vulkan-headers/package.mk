0c3a60d247682ce4d999325a4130f61c00fa7ba0
5schatten <supervisedthinking@gmail.com>
2019-05-29 23:51:28 +0200

    vulkan-headers: updated to v 1.1.108

--- packages/5schatten/drivers/vulkan-headers/package.mk
+++ packages/5schatten/drivers/vulkan-headers/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="vulkan-headers"
-PKG_VERSION="1.1.107"
-PKG_SHA256="7d00748f3311a89d5a92aea4b11764bb8371dae4ab6291497bcde2fa186bbd31"
+PKG_VERSION="1.1.108"
+PKG_SHA256="22f1ea49067cc16635dbb33cc2eeb8358a48d4288b2e587784ba89d90c9ebfce"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Headers"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Headers/archive/v${PKG_VERSION}.tar.gz"
