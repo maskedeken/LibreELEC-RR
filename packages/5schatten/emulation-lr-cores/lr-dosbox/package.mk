0e3a43c7ca9bfada204557615c792f118ef8f730
5schatten <supervisedthinking@gmail.com>
2019-05-31 13:52:25 +0200

    lr-dosbox: added lto build flags for ARM projects

--- packages/5schatten/emulation-lr-cores/lr-dosbox/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-dosbox/package.mk
@@ -6,18 +6,24 @@ PKG_VERSION="2aa0bd13b372956b2fe989ca5e9d8e45f75fb44d"
 PKG_SHA256="15e31cfdb251fae1f2096030227327dcae781640a32cb5f34c23764fb9c7bb75"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/dosbox-libretro"
-PKG_URL="https://github.com/libretro/dosbox-libretro/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/dosbox-libretro/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Port of DOSBox (upstream) to the libretro API."
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="+pic"
 
+configure_package() {
+  if [  "${ARCH}" = "arm" ]; then
+    PKG_BUILD_FLAGS+=" +lto"
+  fi
+}
+
 PKG_LIBNAME="dosbox_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="-f Makefile.libretro GIT_VERSION=${PKG_VERSION:0:7}"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
