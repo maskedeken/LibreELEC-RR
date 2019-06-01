6219e55d624f0b09663e9efbb5f48c1050c8d67e
5schatten <supervisedthinking@gmail.com>
2019-06-01 22:31:22 +0200

    retrofe: updated to git 0941199 / added basic settings.conf

--- packages/5schatten/emulation-frontends/retrofe/package.mk
+++ packages/5schatten/emulation-frontends/retrofe/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="retrofe"
-PKG_VERSION="823c3960e2f26c8f6630774f8a0d4b0cdfa6c806"
-PKG_SHA256="6acfdb6a682d325406579484588650c7c14af755360a07ac72fcb751569fde7c"
+PKG_VERSION="0941199ddec1af942e503e4d27e2a01f4508a2a2" # v0.9.0
+PKG_SHA256="8bcea50111fdceeb27eda0eef4cc816a8b845e0b481fd395b9bb94585b77a602"
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://retrofe.nl/"
 PKG_URL="https://bitbucket.org/phulshof/retrofe/get/${PKG_VERSION}.tar.gz"
@@ -22,23 +22,25 @@ pre_configure_target() {
 
 make_target() {
   cmake --build RetroFE/Build
-  python $PKG_BUILD/Scripts/Package.py --os=linux --build=full
+  python ${PKG_BUILD}/Scripts/Package.py --os=linux --build=full
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/bin
-  mkdir -p $INSTALL/usr/config/retrofe
-  mkdir -p $INSTALL/usr/share/retrofe
+  # Create directories
+  mkdir -p ${INSTALL}/usr/bin
+  mkdir -p ${INSTALL}/usr/config/retrofe
+  mkdir -p ${INSTALL}/usr/share/retrofe
 
   # Install RetroFE resources
-  cp -rf Artifacts/linux/RetroFE/* $INSTALL/usr/config/retrofe
-  mv -v  $INSTALL/usr/config/retrofe/retrofe $INSTALL/usr/bin/
+  cp -rf Artifacts/linux/RetroFE/* ${INSTALL}/usr/config/retrofe
+  mv -v  ${INSTALL}/usr/config/retrofe/retrofe ${INSTALL}/usr/bin/
+  cp -rf ${PKG_DIR}/config/* ${INSTALL}/usr/config/retrofe
 
   # Install scripts
-  cp -rf $PKG_DIR/scripts/common/*     $INSTALL/usr/bin/
-  cp -rf $PKG_DIR/scripts/${PROJECT}/* $INSTALL/usr/bin/
+  cp -rf ${PKG_DIR}/scripts/common/*     ${INSTALL}/usr/bin/
+  cp -rf ${PKG_DIR}/scripts/${PROJECT}/* ${INSTALL}/usr/bin/
 
   # Clean up
-  safe_remove $INSTALL/usr/config/retrofe/README-UBUNTU.txt
-  safe_remove $INSTALL/usr/config/retrofe/README.txt
+  safe_remove ${INSTALL}/usr/config/retrofe/README-UBUNTU.txt
+  safe_remove ${INSTALL}/usr/config/retrofe/README.txt
 }
