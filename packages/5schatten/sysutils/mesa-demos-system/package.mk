6dd0b9468a06efa2a442138767431e00fa19b047
5schatten <supervisedthinking@gmail.com>
2019-06-02 10:11:30 +0200

    mesa-demos-system: changed pkg url to gitlab repo / clean up

--- packages/5schatten/sysutils/mesa-demos-system/package.mk
+++ packages/5schatten/sysutils/mesa-demos-system/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa-demos-system"
-PKG_VERSION="317f67fe5e75c685330d536f158acf6260b473d1"
-PKG_SHA256="be6acbe9a33f745eae1bfc4a57673ce770f257315b8e2e6cb801bbe630a85e33"
+PKG_VERSION="8.4.0"
+PKG_SHA256="84338ce936fa110232a62bcd36c7c2c8710c4a280e9a81c4d10d06f6c2506b7d"
 PKG_ARCH="x86_64"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
-PKG_URL="https://cgit.freedesktop.org/mesa/demos/snapshot/$PKG_VERSION.tar.xz"
+PKG_URL="https://gitlab.freedesktop.org/mesa/demos/-/archive/mesa-demos-${PKG_VERSION}/demos-mesa-demos-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libX11 mesa glu glew"
 PKG_LONGDESC="Mesa 3D demos - installed are the well known glxinfo and glxgears."
 PKG_TOOLCHAIN="autotools"
@@ -15,8 +15,7 @@ PKG_TOOLCHAIN="autotools"
 PKG_CONFIGURE_OPTS_TARGET="--without-glut"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/bin
-  cp src/xdemos/glxinfo $INSTALL/usr/bin
-  cp src/xdemos/glxgears $INSTALL/usr/bin
+  mkdir -p ${INSTALL}/usr/bin
+  cp -v src/xdemos/glxinfo  ${INSTALL}/usr/bin
+  cp -v src/xdemos/glxgears ${INSTALL}/usr/bin
 }
-
