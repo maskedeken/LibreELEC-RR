90dc588791412721d7157408229ba0eb45891bbf
5schatten <supervisedthinking@gmail.com>
2019-06-14 00:11:36 +0200

    lr-mupen64plus: updated to git e64ef9d

--- packages/5schatten/emulation-lr-cores/lr-mupen64plus/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-mupen64plus/package.mk
@@ -2,17 +2,17 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-mupen64plus"
-PKG_VERSION="be81697cd2cdee3a8488eac6789b984975006730"
-PKG_SHA256="2ab7255f8fb32f74eb7dd757a755914e29526ddb3536cfef065c26cf37870d88"
+PKG_VERSION="e64ef9d9f214e32341fb7cd9633260fbb44b2326"
+PKG_SHA256="947abfb1d5ff34d6b22fecfb2df02bd3759fde3f4a0e5d238f65ec7e97d964d4"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mupen64plus-libretro"
-PKG_URL="https://github.com/libretro/mupen64plus-libretro/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/mupen64plus-libretro/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc zlib"
 PKG_LONGDESC="Mupen64Plus is mupen64plus + GLideN64 + libretro"
 PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="mupen64plus_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
 
