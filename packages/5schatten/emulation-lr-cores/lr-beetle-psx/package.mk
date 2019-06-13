47f5ab7889f737a3f5b508119768fa1769e84bd3
5schatten <supervisedthinking@gmail.com>
2019-06-14 00:08:07 +0200

    lr-beetle-psx: updated to git aae54be

--- packages/5schatten/emulation-lr-cores/lr-beetle-psx/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-beetle-psx/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-beetle-psx"
-PKG_VERSION="53591985319edc34d83a0858ad9a935b934dcf5c"
-PKG_SHA256="5c9ae8e49622b95df6485dffe75f90e9282542a3b6d7b5331a8e48287b355302"
+PKG_VERSION="aae54be182bb07b097e6bbbea4e07d1aa4eaf666"
+PKG_SHA256="ce61cb0fdbe5337222ddf06c7a4c6a91388f4f3fa2ee0bc9fb0d7e34dfd26cbf"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-psx-libretro"
 PKG_URL="https://github.com/libretro/beetle-psx-libretro/archive/${PKG_VERSION}.tar.gz"
