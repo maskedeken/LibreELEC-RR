6ed69554fbdfbae41fae47ffd4b86e3ad1363087
5schatten <supervisedthinking@gmail.com>
2019-06-29 16:15:21 +0200

    lr-ppsspp: updated to git 528d04d

--- packages/5schatten/emulation-lr-cores/lr-ppsspp/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-ppsspp/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="lr-ppsspp"
-PKG_VERSION="53e8263371c2c48795c48666a3d5d6c3ca4e8d58" #v1.8.0+
+PKG_VERSION="528d04defeab67e1a7d307a6c97130d40845b358" #v1.8.0+
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/hrydgard/ppsspp"
 PKG_URL="https://github.com/hrydgard/ppsspp.git"
