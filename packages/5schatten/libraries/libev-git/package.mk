19798e89445bdd388d2f46d8fc583af2e35c4610
5schatten <supervisedthinking@gmail.com>
2019-07-03 00:21:14 +0200

    libev-git: updated to v4.27

--- packages/5schatten/libraries/libev-git/package.mk
+++ packages/5schatten/libraries/libev-git/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="libev-git"
-PKG_VERSION="4.25"
-PKG_SHA256="ef2b4031e03b540c510ae373949bcbb2e4335d1bdb20e8975a7ae4153464c99f"
+PKG_VERSION="4.27"
+PKG_SHA256="4c91eaffcf2dde23aa7b459bd9905fea81df7547106a47cfd6eba342c5ce3503"
 PKG_LICENSE="GPL"
 PKG_SITE="http://libev.schmorp.de"
 PKG_URL="https://git.lighttpd.net/libev.git/snapshot/libev-rel-${PKG_VERSION}.tar.gz"
