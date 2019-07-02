c1e3a55b2734229079f02f044834fe76afbb7b13
5schatten <supervisedthinking@gmail.com>
2019-07-02 21:45:55 +0200

    libevent: updated to v2.1.10-stable

--- packages/5schatten/libraries/libevent/package.mk
+++ packages/5schatten/libraries/libevent/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="libevent"
-PKG_VERSION="2.1.9-beta"
-PKG_SHA256="0ab250abac1def3d1e20e23e05ce827efa81db65c9004ccfff58d16404e3e369"
+PKG_VERSION="2.1.10-stable"
+PKG_SHA256="52c9db0bc5b148f146192aa517db0762b2a5b3060ccc63b2c470982ec72b9a79"
 PKG_LICENSE="BSD"
 PKG_SITE="http://libevent.org/"
 PKG_URL="https://github.com/libevent/libevent/archive/release-${PKG_VERSION}.tar.gz"
