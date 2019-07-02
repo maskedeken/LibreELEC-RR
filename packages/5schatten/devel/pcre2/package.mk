6552e8a7b2fda14a3e6f3772ba5896537f64c5ee
5schatten <supervisedthinking@gmail.com>
2019-07-03 00:29:36 +0200

    pcre2: updated to 10.33 / clean up

--- packages/5schatten/devel/pcre2/package.mk
+++ packages/5schatten/devel/pcre2/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="pcre2"
-PKG_VERSION="10.32"
-PKG_SHA256="f29e89cc5de813f45786580101aaee3984a65818631d4ddbda7b32f699b87c2e"
+PKG_VERSION="10.33"
+PKG_SHA256="35514dff0ccdf02b55bd2e9fa586a1b9d01f62332c3356e379eabb75f789d8aa"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.pcre.org/"
-PKG_URL="https://ftp.pcre.org/pub/pcre/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="https://ftp.pcre.org/pub/pcre/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="The PCRE library is a set of functions that implement regular expression pattern matching using the same syntax and semantics as Perl 5"
 PKG_BUILD_FLAGS="+pic"
@@ -17,5 +17,5 @@ PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=OFF \
                        -DPCRE2_SUPPORT_LIBREADLINE=OFF"
 
 post_makeinstall_target() {
-  rm -rf $INSTALL/usr/bin
+  safe_remove ${INSTALL}/usr/bin
 }
