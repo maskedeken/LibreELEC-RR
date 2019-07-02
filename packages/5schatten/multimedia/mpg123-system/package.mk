41c51759668b63c0c0e0a836bfe5b61e2c581fda
5schatten <supervisedthinking@gmail.com>
2019-07-03 00:15:31 +0200

    mpg123-system: updated name / dropped SDL2 dependency

--- packages/5schatten/multimedia/mpg123-system/package.mk
+++ packages/5schatten/multimedia/mpg123-system/package.mk
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
-PKG_NAME="mpg123-compat"
+PKG_NAME="mpg123-system"
 PKG_VERSION="1.25.10"
 PKG_SHA256="6c1337aee2e4bf993299851c70b7db11faec785303cfca3a5c3eb5f329ba7023"
 PKG_LICENSE="LGPLv2"
 PKG_SITE="http://www.mpg123.org/"
-PKG_URL="http://downloads.sourceforge.net/sourceforge/mpg123/mpg123-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2-git"
+PKG_URL="http://downloads.sourceforge.net/sourceforge/mpg123/mpg123-${PKG_VERSION}.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain alsa-lib"
 PKG_LONGDESC="A console based real time MPEG Audio Player for Layer 1, 2 and 3."
