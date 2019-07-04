57970f42aeb770e53274ed3f75b99c0e7f26cf77
5schatten <supervisedthinking@gmail.com>
2019-07-04 23:45:16 +0200

    x264: updated to git 72db437 / disable lto for ARM projects / cleaned up

--- packages/addons/addon-depends/ffmpegx-depends/x264/package.mk
+++ packages/addons/addon-depends/ffmpegx-depends/x264/package.mk
@@ -2,24 +2,25 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="x264"
-PKG_VERSION="545de2ffec6ae9a80738de1b2c8cf820249a2530"
-PKG_SHA256="74725cf7036b2c96387c2c014ef00d181942d00230f21e16277f11d2d9683adc"
+PKG_VERSION="72db437770fd1ce3961f624dd57a8e75ff65ae0b"
+PKG_SHA256="d946f0d79e9f4a56b93adf026b18a6d7ba9d4889bcef37a90013e3e73a152d7b"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.videolan.org/developers/x264.html"
-PKG_URL="http://repo.or.cz/x264.git/snapshot/$PKG_VERSION.tar.gz"
+PKG_URL="http://repo.or.cz/x264.git/snapshot/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="x264 codec"
+PKG_LONGDESC="x264 is a free library for encoding H264/AVC video streams."
 
-if [ "$TARGET_ARCH" = "x86_64" ]; then
+if [ "${TARGET_ARCH}" = "x86_64" ]; then
   PKG_DEPENDS_TARGET+=" nasm:host"
 fi
 
 pre_configure_target() {
-  cd $PKG_BUILD
-  rm -rf .$TARGET_NAME
+  cd ${PKG_BUILD}
+  rm -rf .${TARGET_NAME}
 
-  if [ "$TARGET_ARCH" = "x86_64" ]; then
-    export AS="$TOOLCHAIN/bin/nasm"
+  if [ "${TARGET_ARCH}" = "x86_64" ]; then
+    export AS="${TOOLCHAIN}/bin/nasm"
+    PKG_X264_LTO="--enable-lto"
   else
     PKG_X264_ASM="--disable-asm"
   fi
@@ -27,15 +28,15 @@ pre_configure_target() {
 
 configure_target() {
   ./configure \
-    --cross-prefix="$TARGET_PREFIX" \
-    --extra-cflags="$CFLAGS" \
-    --extra-ldflags="$LDFLAGS" \
-    --host="$TARGET_NAME" \
+    --cross-prefix="${TARGET_PREFIX}" \
+    --extra-cflags="${CFLAGS}" \
+    --extra-ldflags="${LDFLAGS}" \
+    --host="${TARGET_NAME}" \
     --prefix="/usr" \
-    --sysroot="$SYSROOT_PREFIX" \
-    $PKG_X264_ASM \
+    --sysroot="${SYSROOT_PREFIX}" \
+    ${PKG_X264_ASM} \
     --disable-cli \
-    --enable-lto \
+    ${PKG_X264_LTO} \
     --enable-pic \
     --enable-static \
     --enable-strip
