f3d0935699a603d4d7087064b57b39b33f0a63bc
5schatten <supervisedthinking@gmail.com>
2019-07-08 21:17:55 +0200

    mame2010-lr: updated to git c66718f

--- packages/5schatten/emulation/libretro/mame2010-lr/package.mk
+++ packages/5schatten/emulation/libretro/mame2010-lr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="mame2010-lr"
-PKG_VERSION="c87309dab5fc8db292e31737fcd1754de1fbae6a"
-PKG_SHA256="6f442b362b5929f8b762f289a874e2011902617efdcea4bfc2fc2a54836973ec"
+PKG_VERSION="c66718f3670ef8d5a94a9a2cadfcccf46061a894"
+PKG_SHA256="a0f2242a55534834dfdd59c110b5ee9b7d56f791a7a3a9bde981551d44fdf5f5"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame2010-libretro"
 PKG_URL="https://github.com/libretro/mame2010-libretro/archive/${PKG_VERSION}.tar.gz"
