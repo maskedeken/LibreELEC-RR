2a25ba338c798b50fe9f2118273488ee865d1409
5schatten <supervisedthinking@gmail.com>
2019-07-09 12:05:08 +0200

    speexdsp: initial package v1.2.0

---
+++ packages/5schatten/audio/speexdsp/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="speexdsp"
+PKG_VERSION="1.2.0"
+PKG_SHA256="d7032f607e8913c019b190c2bccc36ea73fc36718ee38b5cdfc4e4c0a04ce9a4"
+PKG_LICENSE="BSD"
+PKG_SITE="https://speex.org"
+PKG_URL="https://github.com/xiph/speexdsp/archive/SpeexDSP-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="SpeexDSP is a patent-free, Open Source/Free Software DSP library."
+PKG_TOOLCHAIN="autotools"
