a47632af5606053e74375a464afda15567a8e8cd
5schatten <supervisedthinking@gmail.com>
2019-07-15 17:12:12 +0200

    genesis-plus-gx-lr: updated to git e51ab09

--- packages/5schatten/emulation/libretro/genesis-plus-gx-lr/package.mk
+++ packages/5schatten/emulation/libretro/genesis-plus-gx-lr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="genesis-plus-gx-lr"
-PKG_VERSION="de4804cf508053394c308453ab4e2aec95c6f9a5"
-PKG_SHA256="878c638455b4b53940c5d771e55941e53d011556ca539912501ac7c03589b569"
+PKG_VERSION="e51ab09850a940dca9a11bb210fc9e2c778ec206"
+PKG_SHA256="500c12c7aa0c984ce9093230aa3ecf8961e6dd985b6e211af57d7307c7dd5f9a"
 PKG_LICENSE="Modified BSD / LGPLv2.1"
 PKG_SITE="https://github.com/libretro/Genesis-Plus-GX"
 PKG_URL="https://github.com/libretro/Genesis-Plus-GX/archive/${PKG_VERSION}.tar.gz"
