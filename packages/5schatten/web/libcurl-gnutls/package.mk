301e4fb6350d2f2a127a35773a5dc8612b78bde7
5schatten <supervisedthinking@gmail.com>
2019-07-19 13:12:14 +0200

    libcurl-gnutls: updated to 7.65.3 / fixed linking against OpenSSL / added http2 / clean up

--- packages/5schatten/web/libcurl-gnutls/package.mk
+++ packages/5schatten/web/libcurl-gnutls/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="libcurl-gnutls"
-PKG_VERSION="7.65.1"
-PKG_SHA256="f6c22074877f235aebc7c53057dbc7ee82358f8ae58bfb767e955c18c859a77a"
+PKG_VERSION="7.65.3"
+PKG_SHA256="f2d98854813948d157f6a91236ae34ca4a1b4cb302617cebad263d79b0235fea"
 PKG_LICENSE="MIT"
 PKG_SITE="http://curl.haxx.se"
 PKG_URL="http://curl.haxx.se/download/curl-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain glibc zlib gnutls rtmpdump nettle libidn2"
-PKG_LONGDESC="libcurl without versioned symbols"
+PKG_DEPENDS_TARGET="toolchain glibc zlib gnutls rtmpdump nettle libidn2 nghttp2"
+PKG_LONGDESC="An URL retrieval library (linked against gnutls)"
 PKG_TOOLCHAIN="configure"
 PKG_BUILD_FLAGS="-gold"
 
@@ -55,20 +55,18 @@ pre_configure_target() {
                              --without-egd-socket \
                              --enable-thread \
                              --with-random=/dev/urandom \
-                             --with-gnutls=${SYSROOT_PREFIX}/usr \
+                             --with-gnutls \
                              --without-ssl \
-                             --without-polarssl \
+                             --without-mbedtls \
                              --without-nss \
-                             --with-ca-bundle=/etc/ssl/cert.pem \
+                             --with-ca-bundle=/run/libreelec/cacert.pem \
                              --without-ca-path \
                              --without-libpsl \
                              --without-libmetalink \
                              --without-libssh2 \
-                             --with-librtmp=${SYSROOT_PREFIX}/usr \
-                             --without-libidn"
-
-  # link against librt because of undefined reference to 'clock_gettime'
-  export LIBS="-lrt -lm -lrtmp -lssl"
+                             --with-librtmp \
+                             --with-libidn2 \
+                             --with-nghttp2"
 }
 
 makeinstall_target() {
@@ -80,4 +78,4 @@ makeinstall_target() {
   for version in 3 4 4.0.0 4.1.0 4.2.0 4.3.0 4.4.0; do
     ln -s libcurl-gnutls.so.4.5.0 ${INSTALL}/usr/lib/libcurl-gnutls.so.${version}
   done
-}	
+}
