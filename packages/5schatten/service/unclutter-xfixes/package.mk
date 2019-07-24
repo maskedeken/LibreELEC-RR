6a93adf55ccd1449f6121807ae7ce064c0b94291
5schatten <supervisedthinking@gmail.com>
2019-07-25 00:58:36 +0200

    unclutter-xfixes: updated dependencies

--- packages/5schatten/service/unclutter-xfixes/package.mk
+++ packages/5schatten/service/unclutter-xfixes/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="35c75ad24be989dd6708db1d9ce9b2a2f814b80638c0633cdb075c6df090ed11"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/Airblader/unclutter-xfixes"
 PKG_URL="https://github.com/Airblader/unclutter-xfixes/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libX11 libXi libXfixes libev-git"
+PKG_DEPENDS_TARGET="toolchain libX11 libXi libXfixes libev-system"
 PKG_LONGDESC="This is a rewrite of the popular tool unclutter, but using the x11-xfixes extension."
 
 post_install() {
