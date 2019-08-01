ae4dfd572ff76f348ccd6d49b4aea5cb620bd6bc
5schatten <supervisedthinking@gmail.com>
2019-08-01 20:22:22 +0200

    nestopia: updated to git cbe1444

--- packages/5schatten/emulation/libretro/nestopia/package.mk
+++ packages/5schatten/emulation/libretro/nestopia/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="nestopia"
-PKG_VERSION="a8e268b547e7a44cc47b99dc4141d2e38b7978ec"
-PKG_SHA256="7e0e7d75774a39ed4cd77827e88ec69dacf0260312c7bada9533463db931ce0d"
+PKG_VERSION="cbe14441d666a9cfad6f0aa3325ea23502dce123"
+PKG_SHA256="639032c54c0e18b25798a9a712d0c071def3d97309e861410c44eee3c7d1085e"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/nestopia"
 PKG_URL="https://github.com/libretro/nestopia/archive/${PKG_VERSION}.tar.gz"
