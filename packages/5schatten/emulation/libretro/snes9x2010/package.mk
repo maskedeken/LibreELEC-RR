c53a327ad2d733b47624ed1e5b4ae36d7228ad3e
5schatten <supervisedthinking@gmail.com>
2019-08-02 18:35:00 +0200

    snes9x2010: updated to git b6d4b2f

--- packages/5schatten/emulation/libretro/snes9x2010/package.mk
+++ packages/5schatten/emulation/libretro/snes9x2010/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="snes9x2010"
-PKG_VERSION="01671616de0f5eaaea92aabb4ea33b1a57107949"
-PKG_SHA256="7697a693925c95b1a222530b81961776d31da38446e3cafe0095937c172b6d53"
+PKG_VERSION="b6d4b2fac07f6903c358c0bdf4b0a75c452575d6"
+PKG_SHA256="3f908246a1b3d72aec95cf106e54330325adccb146b0fb7524fcfdb3f09a064a"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/snes9x2010"
 PKG_URL="https://github.com/libretro/snes9x2010/archive/${PKG_VERSION}.tar.gz"
