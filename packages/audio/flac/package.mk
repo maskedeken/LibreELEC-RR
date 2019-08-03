4d92c9244facce622f689b68e4850a45632ff93f
5schatten <supervisedthinking@gmail.com>
2019-08-03 18:07:40 +0200

    flac: reverted changes / restored upstream package

--- packages/audio/flac/package.mk
+++ packages/audio/flac/package.mk
@@ -14,7 +14,9 @@ PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+pic"
 
 # package specific configure options
-PKG_CONFIGURE_OPTS_TARGET="--disable-rpath \
+PKG_CONFIGURE_OPTS_TARGET="--enable-static \
+                           --disable-shared \
+                           --disable-rpath \
                            --disable-altivec \
                            --disable-doxygen-docs \
                            --disable-thorough-tests \
