30e33472978e685763816ef4825acfdd98dedbbb
5schatten <supervisedthinking@gmail.com>
2019-08-08 20:46:03 +0200

    citra: updated to git 3e9c2e7

--- packages/5schatten/emulation/standalone/citra/package.mk
+++ packages/5schatten/emulation/standalone/citra/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="citra"
-PKG_VERSION="76aec4d675b81f6bb193d9e974604cc31d355c56"
+PKG_VERSION="3e9c2e77d929525100f0b8b31ae583a53ef2c2dd"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2+"
 PKG_SITE="https://github.com/citra-emu/citra"
