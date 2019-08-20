5c14e95a03d5516f4462a61cd44ab609f8c2ef7d
5schatten <supervisedthinking@gmail.com>
2019-08-20 21:12:05 +0200

    ppsspp: updated to git 19ba77f

--- packages/5schatten/emulation/standalone/ppsspp/package.mk
+++ packages/5schatten/emulation/standalone/ppsspp/package.mk
@@ -11,7 +11,7 @@ GET_HANDLER_SUPPORT="git"
 PKG_BUILD_FLAGS="+lto"
 
 if [ "${PROJECT}" = "Generic" ]; then
-  PKG_VERSION="3356f9402932630ead5887b907b5ccbeb17d09cf" #v1.8.0+
+  PKG_VERSION="19ba77f223f8d29bc3a12bdd4448bc9c576e2bdb" #v1.8.0+
 else
   PKG_VERSION="74d87fa2b4a3c943c1df09cc26a8c70b1335fd30" #v1.7.5
 fi
