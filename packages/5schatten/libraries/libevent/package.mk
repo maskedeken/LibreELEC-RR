f953bf205bb0bb723e5d64ea1ba0481764684448
5schatten <supervisedthinking@gmail.com>
2019-08-20 11:55:25 +0200

    libevent: updated to v2.1.11-stable

--- packages/5schatten/libraries/libevent/package.mk
+++ packages/5schatten/libraries/libevent/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="libevent"
-PKG_VERSION="2.1.10-stable"
-PKG_SHA256="52c9db0bc5b148f146192aa517db0762b2a5b3060ccc63b2c470982ec72b9a79"
+PKG_VERSION="2.1.11-stable"
+PKG_SHA256="229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
 PKG_LICENSE="BSD"
 PKG_SITE="http://libevent.org/"
 PKG_URL="https://github.com/libevent/libevent/archive/release-${PKG_VERSION}.tar.gz"
@@ -13,8 +13,5 @@ PKG_LONGDESC="Event notification library."
 PKG_CMAKE_OPTS_TARGET="-DEVENT__DISABLE_DEBUG_MODE=on \
                        -DEVENT__DISABLE_BENCHMARK=on \
                        -DEVENT__DISABLE_TESTS=on \
+                       -DEVENT__LIBRARY_TYPE=SHARED \
                        -DEVENT__DISABLE_SAMPLES=on"
-
-makeinstall_target() {
- :
-}
