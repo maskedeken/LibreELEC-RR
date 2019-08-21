81f976acd1af3bac30f61cbb0dcd6e33afb767f4
5schatten <supervisedthinking@gmail.com>
2019-08-21 12:46:31 +0200

    vulkan-loader: updated to v1.1.120

--- packages/5schatten/graphics/vulkan/vulkan-loader/package.mk
+++ packages/5schatten/graphics/vulkan/vulkan-loader/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="vulkan-loader"
-PKG_VERSION="1.1.119"
-PKG_SHA256="43bd7d370291603dbdd02a61015bb2c3bbd7f6751b2dca3e64aa5c4e5810449d"
+PKG_VERSION="1.1.120"
+PKG_SHA256="5cc0bedde46fca69da21f2bc5197de5b07f1bea1b70fedd6d6724230a4ba6fb2"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/v${PKG_VERSION}.tar.gz"
