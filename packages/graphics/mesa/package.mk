74b0ec4fad638ab6d40f1c0d53afd9f2289e7a0a
5schatten <supervisedthinking@gmail.com>
2019-08-23 20:03:13 +0200

    mesa: fix build & clean up

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -7,17 +7,10 @@ PKG_VERSION="19.2.0-rc1"
 PKG_SHA256="e4d93e91a8ced0b4d068a5b2f9eee978a221ad20bb1284944d2ce89b1cdb9cad"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
-PKG_URL="https://github.com/mesa3d/mesa/archive/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/mesa3d/mesa/archive/mesa-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain expat libdrm Mako:host"
-PKG_LONGDESC="Mesa is a 3-D graphics library with an API."
+PKG_LONGDESC="The Mesa 3D Graphics Library, is an open source software implementation of OpenGL, Vulkan, and other graphics API specifications."
 PKG_TOOLCHAIN="meson"
-PKG_BUILD_FLAGS="+lto"
-
-if listcontains "${GRAPHIC_DRIVERS}" "(lima|panfrost)"; then
-  PKG_VERSION="89381191a96891a61a1f98102780669990b9f626" # master-19.2
-  PKG_SHA256="844c53b9c955af8f4da212e66aab2533cd964b376cc33f3c3999f2d0cacf998d"
-  PKG_URL="https://gitlab.freedesktop.org/mesa/mesa/-/archive/$PKG_VERSION/mesa-$PKG_VERSION.tar.gz"
-fi
 
 get_graphicdrivers
 
@@ -34,7 +27,6 @@ PKG_MESON_OPTS_TARGET="-Ddri-drivers=${DRI_DRIVERS// /,} \
                        -Dgbm=true \
                        -Degl=true \
                        -Dglvnd=false \
-                       -Dasm=true \
                        -Dvalgrind=false \
                        -Dlibunwind=false \
                        -Dlmsensors=false \
@@ -42,53 +34,53 @@ PKG_MESON_OPTS_TARGET="-Ddri-drivers=${DRI_DRIVERS// /,} \
                        -Dselinux=false \
                        -Dosmesa=none"
 
-if [ "$DISPLAYSERVER" = "x11" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET xorgproto libXext libXdamage libXfixes libXxf86vm libxcb libX11 libxshmfence libXrandr"
+if [ "${DISPLAYSERVER}" = "x11" ]; then
+  PKG_DEPENDS_TARGET+=" xorgproto libXext libXdamage libXfixes libXxf86vm libxcb libX11 libxshmfence libXrandr"
   export X11_INCLUDES=
   PKG_MESON_OPTS_TARGET+=" -Dplatforms=x11,drm -Ddri3=true -Dglx=dri"
-elif [ "$DISPLAYSERVER" = "weston" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET wayland wayland-protocols"
+elif [ "${DISPLAYSERVER}" = "weston" ]; then
+  PKG_DEPENDS_TARGET+=" wayland wayland-protocols"
   PKG_MESON_OPTS_TARGET+=" -Dplatforms=wayland,drm -Ddri3=false -Dglx=disabled"
 else
   PKG_MESON_OPTS_TARGET+=" -Dplatforms=drm -Ddri3=false -Dglx=disabled"
 fi
 
-if [ "$LLVM_SUPPORT" = "yes" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET elfutils llvm"
-  export LLVM_CONFIG="$SYSROOT_PREFIX/usr/bin/llvm-config-host"
+if [ "${LLVM_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" elfutils llvm"
+  export LLVM_CONFIG="${SYSROOT_PREFIX}/usr/bin/llvm-config-host"
   PKG_MESON_OPTS_TARGET+=" -Dllvm=true"
 else
   PKG_MESON_OPTS_TARGET+=" -Dllvm=false"
 fi
 
-if [ "$VDPAU_SUPPORT" = "yes" -a "$DISPLAYSERVER" = "x11" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libvdpau"
+if [ "${VDPAU_SUPPORT}" = "yes" -a "${DISPLAYSERVER}" = "x11" ]; then
+  PKG_DEPENDS_TARGET+=" libvdpau"
   PKG_MESON_OPTS_TARGET+=" -Dgallium-vdpau=true"
 else
   PKG_MESON_OPTS_TARGET+=" -Dgallium-vdpau=false"
 fi
 
-if [ "$VAAPI_SUPPORT" = "yes" ] && listcontains "$GRAPHIC_DRIVERS" "(r600|radeonsi)"; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libva"
+if [ "${VAAPI_SUPPORT}" = "yes" ] && listcontains "${GRAPHIC_DRIVERS}" "(r600|radeonsi)"; then
+  PKG_DEPENDS_TARGET+=" libva"
   PKG_MESON_OPTS_TARGET+=" -Dgallium-va=true"
 else
   PKG_MESON_OPTS_TARGET+=" -Dgallium-va=false"
 fi
 
-if listcontains "$GRAPHIC_DRIVERS" "vmware"; then
+if listcontains "${GRAPHIC_DRIVERS}" "vmware"; then
   PKG_MESON_OPTS_TARGET+=" -Dgallium-xa=true"
 else
   PKG_MESON_OPTS_TARGET+=" -Dgallium-xa=false"
 fi
 
-if [ "$OPENGLES_SUPPORT" = "yes" ]; then
+if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
   PKG_MESON_OPTS_TARGET+=" -Dgles1=true -Dgles2=true"
 else
   PKG_MESON_OPTS_TARGET+=" -Dgles1=false -Dgles2=false"
 fi
 
-if [ "$VULKAN_SUPPORT" = "yes" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET vulkan-loader"
+if [ "${VULKAN_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" vulkan-loader"
   PKG_MESON_OPTS_TARGET+=" -Dvulkan-drivers=amd,intel"
 else
   PKG_MESON_OPTS_TARGET+=" -Dvulkan-drivers="
@@ -99,7 +91,7 @@ fi
 # dependency tracking. The following has some ideas on how to address that.
 # https://github.com/LibreELEC/LibreELEC.tv/pull/2163
 pre_configure_target() {
-  if [ "$DISPLAYSERVER" = "x11" ]; then
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
     export LIBS="-lxcb-dri3 -lxcb-dri2 -lxcb-xfixes -lxcb-present -lxcb-sync -lxshmfence -lz"
   fi
 
@@ -108,7 +100,7 @@ pre_configure_target() {
   # arm userspace. The hack does not affect building for aarch64.
   if [[ "${MALI_FAMILY}" = *t8* ]]; then
     (
-      cd "$PKG_BUILD/src/gallium/drivers/panfrost"
+      cd "${PKG_BUILD}/src/gallium/drivers/panfrost"
       sed -i 's/uintptr_t/uint64_t/g' include/panfrost-job.h \
                                       include/panfrost-misc.h \
                                       pan_context.c \
@@ -121,12 +113,12 @@ pre_configure_target() {
 
 post_makeinstall_target() {
   # Similar hack is needed on EGL, GLES* front. Might as well drop it and test the GLVND?
-  if [ "$DISPLAYSERVER" = "x11" ]; then
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
     # rename and relink for cooperate with nvidia drivers
-    rm -rf $INSTALL/usr/lib/libGL.so
-    rm -rf $INSTALL/usr/lib/libGL.so.1
-    ln -sf libGL.so.1 $INSTALL/usr/lib/libGL.so
-    ln -sf /var/lib/libGL.so $INSTALL/usr/lib/libGL.so.1
-    mv $INSTALL/usr/lib/libGL.so.1.2.0 $INSTALL/usr/lib/libGL_mesa.so.1
+    safe_remove ${INSTALL}/usr/lib/libGL.so
+    safe_remove ${INSTALL}/usr/lib/libGL.so.1
+    ln -sf libGL.so.1 ${INSTALL}/usr/lib/libGL.so
+    ln -sf /var/lib/libGL.so ${INSTALL}/usr/lib/libGL.so.1
+    mv ${INSTALL}/usr/lib/libGL.so.1.2.0 ${INSTALL}/usr/lib/libGL_mesa.so.1
   fi
 }
