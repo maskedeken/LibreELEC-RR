3178e479fea15ed3b5eeeb7bc46b72ac22033671
5schatten <supervisedthinking@gmail.com>
2019-08-25 00:33:13 +0200

    vulkan-tools: moved to graphics folder / clean up

---
+++ packages/5schatten/graphics/vulkan/vulkan-tools/package.mk
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="vulkan-tools"
+PKG_VERSION="1.1.120"
+PKG_SHA256="888b2f60bba8c89174dfd8d88a7f072ba497166aede5027e33e914e9265cbc2a"
+PKG_LICENSE="Apache 2.0"
+PKG_SITE="https://github.com/KhronosGroup/Vulkan-Tools"
+PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain cmake:host vulkan-loader"
+PKG_LONGDESC="This project provides Khronos official Vulkan Tools and Utilities."
+
+pre_configure_target() {
+  PKG_CMAKE_OPTS_TARGET="-DBUILD_ICD=Off \
+                         -DINSTALL_ICD=Off"
+
+  # Disable Wayland WSI support
+  sed -e "s/Build Wayland WSI support\" ON/Build Wayland WSI support\" OFF/" -i ${PKG_BUILD}/cube/CMakeLists.txt
+  sed -e "s/Build Wayland WSI support\" ON/Build Wayland WSI support\" OFF/" -i ${PKG_BUILD}/vulkaninfo/CMakeLists.txt
+}
+
+pre_make_target() {
+  # Fix cross compiling
+  find ${PKG_BUILD} -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
+}
