88e490a5e0306edc656d9a8d671041c313e92f3d
5schatten <supervisedthinking@gmail.com>
2019-08-24 14:33:25 +0200

    Skyscraper: updated to v3.2.6

--- packages/5schatten/sysutils/Skyscraper/package.mk
+++ packages/5schatten/sysutils/Skyscraper/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="Skyscraper"
-PKG_VERSION="3.2.5"
-PKG_SHA256="3ba74d4be118a9e850a7e8559d01b27ce16f6833f6a46f7ce06908922de47254"
+PKG_VERSION="3.2.6"
+PKG_SHA256="93132c73fdc8954ae44e490a9c9d4b456e8df121e57d819afad656fccc8d94d1"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/muldjord/skyscraper"
 PKG_URL="https://github.com/muldjord/skyscraper/archive/${PKG_VERSION}.tar.gz"
