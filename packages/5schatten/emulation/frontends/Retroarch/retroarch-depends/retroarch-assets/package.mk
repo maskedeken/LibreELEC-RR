5042a3045225cfcbbfb0ff86f9624477c6843b4b
5schatten <supervisedthinking@gmail.com>
2019-08-24 15:01:40 +0200

    retroarch-assets: updated to git ecc2ab7

--- packages/5schatten/emulation/frontends/Retroarch/retroarch-depends/retroarch-assets/package.mk
+++ packages/5schatten/emulation/frontends/Retroarch/retroarch-depends/retroarch-assets/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="493666090e3c06709e4ef44f8beeddf8e82e839a"
-PKG_SHA256="5398e9cab3ee7a6e858e71ed50b1e16a145fb677d33871fd4b1d498553f543ce"
+PKG_VERSION="ecc2ab7a239ff1ecf2c234dcdd7129e5179ae2e6"
+PKG_SHA256="e5e686f31d89b289520af61ddf38477e1338350ada8c0052e374c9ad6ae88a78"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/retroarch-assets"
 PKG_URL="https://github.com/libretro/retroarch-assets/archive/${PKG_VERSION}.tar.gz"
