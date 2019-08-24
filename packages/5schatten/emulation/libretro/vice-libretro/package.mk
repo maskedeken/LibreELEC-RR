5fd74dccc11c87ceb9695b0546c75ea42881455c
5schatten <supervisedthinking@gmail.com>
2019-08-24 15:12:01 +0200

    vice-libretro: updated to git cdbb774

--- packages/5schatten/emulation/libretro/vice-libretro/package.mk
+++ packages/5schatten/emulation/libretro/vice-libretro/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="vice-libretro"
-PKG_VERSION="2ce07099c3dd84ff7174b3c7238b750f02f91048"
-PKG_SHA256="09d6b9a8d89cb077ed332d1f91d0d76ab6ef43a475eff97a30a2e36a037cf985"
+PKG_VERSION="cdbb7745a003651792a38d804ca93a239400659e"
+PKG_SHA256="558ae4638f85ab7a96555802c7b2a980ed5b9607a0cad202909bfb50e0b18eba"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/vice-libretro"
 PKG_URL="https://github.com/libretro/vice-libretro/archive/${PKG_VERSION}.tar.gz"
