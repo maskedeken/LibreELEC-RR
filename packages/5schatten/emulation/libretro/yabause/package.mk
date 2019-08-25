ecceef34c5e1874e6fe91b16744e052e16303949
5schatten <supervisedthinking@gmail.com>
2019-08-25 13:42:55 +0200

    yabause: updated to git 0f1f8ac

--- packages/5schatten/emulation/libretro/yabause/package.mk
+++ packages/5schatten/emulation/libretro/yabause/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="yabause"
-PKG_VERSION="d28d74d928a80371761a8d44ef9c95d08ed5992e"
-PKG_SHA256="46c9fa02cfacd59b9cc3ab700eb8b9c1860c3f3d1e081d3d358d70fd33b1008a"
+PKG_VERSION="0f1f8ac3fa0c4ce813553944d281c18d505a5f5b"
+PKG_SHA256="f28edf976396c02029fbf99ec91598134139982344dcb8a6ca85f466a57edab1"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/yabause"
 PKG_URL="https://github.com/libretro/yabause/archive/${PKG_VERSION}.tar.gz"
