dc7f53183b2b3af78a7cacdfdeedcbe06279c330
5schatten <supervisedthinking@gmail.com>
2019-08-26 16:27:38 +0200

    emulationstation: updated to git d62758c / updated patches

--- packages/5schatten/emulation/frontends/Emulationstation/emulationstation/package.mk
+++ packages/5schatten/emulation/frontends/Emulationstation/emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="4b4c20720810f64904e763e70c00906eb7394b7e" #v2.9.0-dev
+PKG_VERSION="d62758c7cf88154ef05177217e4a0a33bbda02d2" #v2.9.0-dev
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/RetroPie/EmulationStation"
 PKG_URL="https://github.com/RetroPie/EmulationStation.git"
