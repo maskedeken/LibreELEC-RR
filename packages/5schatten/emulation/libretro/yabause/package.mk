953c80a0d73aee84b0bdeed398d1c742bd7a816a
5schatten <supervisedthinking@gmail.com>
2019-08-29 12:08:26 +0200

    yabause: reworked make opts / added armv build fix

--- packages/5schatten/emulation/libretro/yabause/package.mk
+++ packages/5schatten/emulation/libretro/yabause/package.mk
@@ -13,9 +13,24 @@ PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="+lto"
 
 PKG_LIBNAME="yabause_libretro.so"
-PKG_LIBPATH="libretro/${PKG_LIBNAME}"
+PKG_LIBPATH="yabause/src/libretro/${PKG_LIBNAME}"
 
-PKG_MAKE_OPTS_TARGET="-C libretro GIT_VERSION=${PKG_VERSION:0:7}"
+PKG_MAKE_OPTS_TARGET="-C yabause/src/libretro GIT_VERSION=${PKG_VERSION:0:7}"
+
+pre_configure_target() {
+  if [ "${ARCH}" = "arm" ]; then
+    PKG_MAKE_OPTS_TARGET+=" platform=armv"
+    # ARM NEON support
+    if target_has_feature neon; then
+      PKG_MAKE_OPTS_TARGET+="-neon"
+    fi
+    PKG_MAKE_OPTS_TARGET+="-${TARGET_FLOAT}float-${TARGET_CPU}"
+  fi
+}
+
+pre_make_target() {
+  make CC=${HOST_CC} -C yabause/src/libretro generate-files
+}
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
