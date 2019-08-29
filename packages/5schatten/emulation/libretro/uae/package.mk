8905f7d41fa7a734088f766d9b51445e6bf9cd78
5schatten <supervisedthinking@gmail.com>
2019-08-29 12:29:36 +0200

    uae: updated to git a8c8053 / switched toolchain to make / reworked make opts

--- packages/5schatten/emulation/libretro/uae/package.mk
+++ packages/5schatten/emulation/libretro/uae/package.mk
@@ -2,20 +2,32 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="uae"
-PKG_VERSION="0dcc239e445b59309815b2d1f557298a153c45ae"
-PKG_SHA256="4d56509b1ee2cf75e8b948a32b11d1944a286bfb7470281d004ee47b7672a702"
+PKG_VERSION="a8c80532c4bb1551170b9d52ffe82f8e249af605"
+PKG_SHA256="bbd15d193a278b916c0172dd7e9d7fa92983fc31da0f50b7f18406d6f9eeada1"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-uae"
 PKG_URL="https://github.com/libretro/libretro-uae/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain kodi-platform"
 PKG_LONGDESC="Libretro wrapper for UAE emulator."
+PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="puae_libretro.so"
 PKG_LIBPATH="${PKG_LIBNAME}"
 
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
 pre_configure_target() {
   if [ "${ARCH}" = "arm" ]; then
-    CFLAGS+=" -DARM -marm"
+    if [ "${PROJECT}" = "RPi" ]; then
+      PKG_MAKE_OPTS_TARGET+=" platform=rpi"
+    else
+      PKG_MAKE_OPTS_TARGET+=" platform=armv"
+      # ARM NEON support
+      if target_has_feature neon; then
+        PKG_MAKE_OPTS_TARGET+="-neon"
+      fi
+      PKG_MAKE_OPTS_TARGET+="-${TARGET_FLOAT}float-${TARGET_CPU}"
+    fi
   fi
 }
 
