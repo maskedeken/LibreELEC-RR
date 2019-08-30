fef9126315f0b77d9214db982152ae8599f8fb36
5schatten <supervisedthinking@gmail.com>
2019-08-31 01:39:32 +0200

    libglvnd: initial package v1.1.1

---
+++ packages/graphics/libglvnd/package.mk
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libglvnd"
+PKG_VERSION="1.1.1"
+PKG_SHA256="baca5e1a78b96a112650cdc597be3f856d4754eb73a7bf3f6629e78a7e9f2b5a"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/NVIDIA/libglvnd"
+PKG_URL="https://github.com/NVIDIA/libglvnd/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libX11 libXext xorgproto"
+PKG_LONGDESC="libglvnd is a vendor-neutral dispatch layer for arbitrating OpenGL API calls between multiple vendors."
+PKG_TOOLCHAIN="autotools"
+
+if [ "$OPENGLES_SUPPORT" = "no" ]; then
+  PKG_CONFIGURE_OPTS_TARGET="--disable-gles"
+fi
