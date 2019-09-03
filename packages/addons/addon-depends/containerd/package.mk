ffae6a08674d3ce2054b7026fb4b2a2b67cce91b
5schatten <supervisedthinking@gmail.com>
2019-09-03 11:10:05 +0200

    containerd: fixed displayed git commit

--- packages/addons/addon-depends/containerd/package.mk
+++ packages/addons/addon-depends/containerd/package.mk
@@ -12,6 +12,9 @@ PKG_DEPENDS_TARGET="toolchain go:host"
 PKG_LONGDESC="A daemon to control runC, built for performance and density."
 PKG_TOOLCHAIN="manual"
 
+# Git commit of the matching release https://github.com/containerd/containerd/releases
+PKG_GIT_COMMIT="a4bc1d432a2c33aa2eed37f338dceabb93641310"
+
 pre_make_target() {
   case ${TARGET_ARCH} in
     x86_64)
@@ -39,7 +42,7 @@ pre_make_target() {
   export CGO_NO_EMULATION=1
   export CGO_CFLAGS=${CFLAGS}
   export CONTAINERD_VERSION=v${PKG_VERSION}
-  export CONTAINERD_REVISION=${PKG_VERSION}
+  export CONTAINERD_REVISION=${PKG_GIT_COMMIT}
   export CONTAINERD_PKG=github.com/containerd/containerd
   export LDFLAGS="-w -extldflags -static -X ${CONTAINERD_PKG}/version.Version=${CONTAINERD_VERSION} -X ${CONTAINERD_PKG}/version.Revision=${CONTAINERD_REVISION} -X ${CONTAINERD_PKG}/version.Package=${CONTAINERD_PKG} -extld $CC"
   export GOLANG=${TOOLCHAIN}/lib/golang/bin/go
