078a22664d88957b7036900a22783c599e6a8240
5schatten <supervisedthinking@gmail.com>
2019-09-03 22:07:30 +0200

    uae: updated to git fba606e

--- packages/5schatten/emulation/libretro/uae/package.mk
+++ packages/5schatten/emulation/libretro/uae/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="uae"
-PKG_VERSION="943e1f20e04bf690a3ec8f9bae1b333ae4037421"
-PKG_SHA256="45a9a8211fb72b51c309deabf401ca3c58c2e3d6871b9d3b188b5738d27dbb64"
+PKG_VERSION="fba606ec3334a3ced54f91ff754b81b7d27783bb"
+PKG_SHA256="7e9c459c47a592199afe842329dbf368115b5981205a88cbd868dc982e22dba1"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-uae"
 PKG_URL="https://github.com/libretro/libretro-uae/archive/${PKG_VERSION}.tar.gz"
