d47b1ac7e7ae63efcc66d32291c884c3fed50721
5schatten <supervisedthinking@gmail.com>
2019-09-09 19:55:53 +0200

    citra: updated to git 24ec11f / dropped externals/fmt gcc9 fix

--- packages/5schatten/emulation/standalone/citra/package.mk
+++ packages/5schatten/emulation/standalone/citra/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="citra"
-PKG_VERSION="8acd2b966d8f2bdf72074c63b30c3e03bb696d06"
+PKG_VERSION="24ec11f9285755935b65f8e86a4e8141fa80a30f"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2+"
 PKG_SITE="https://github.com/citra-emu/citra"
@@ -20,9 +20,6 @@ pre_configure_target() {
                          -DENABLE_WEB_SERVICE=0 \
                          -DCMAKE_NO_SYSTEM_FROM_IMPORTED=1 \
                          -DCMAKE_VERBOSE_MAKEFILE=1"
-
-  # Workaround for GCC 9.1.0 https://github.com/citra-emu/citra/issues/4766
-  CXXFLAGS+=" -DFMT_USE_USER_DEFINED_LITERALS=0"
 }
 
 pre_make_target() {
