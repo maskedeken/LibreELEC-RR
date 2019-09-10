d535b322d82f9761e380d5a7c319de4897878539
5schatten <supervisedthinking@gmail.com>
2019-09-10 11:18:21 +0200

    core-info-lr: updated to v1.7.8-3

--- packages/5schatten/emulation/frontends/Retroarch/retroarch-depends/core-info-lr/package.mk
+++ packages/5schatten/emulation/frontends/Retroarch/retroarch-depends/core-info-lr/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="core-info-lr"
-PKG_VERSION="aa957733639cc234b6e7b5b0b564ed38042df08b"
-PKG_SHA256="71b42771f2ae17ec5ffeeb6795690a2afed01d82c3239aba9144395f2cb183b7"
+PKG_VERSION="1.7.8-3"
+PKG_SHA256="2fc1b5de49fd8ad594cfd6df3e5ecae3b62cec6586ed12f32b0fc4670bf7f4bb"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-core-info"
-PKG_URL="https://github.com/libretro/libretro-core-info/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/libretro/libretro-core-info/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Libretro's core info files"
 PKG_TOOLCHAIN="manual"
