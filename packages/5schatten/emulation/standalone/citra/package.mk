d02c532967f503c524466d1c713ca949cc4975ca
5schatten <supervisedthinking@gmail.com>
2019-09-11 21:45:13 +0200

    citra: updated to git d6f2bc8

--- packages/5schatten/emulation/standalone/citra/package.mk
+++ packages/5schatten/emulation/standalone/citra/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="citra"
-PKG_VERSION="24ec11f9285755935b65f8e86a4e8141fa80a30f"
+PKG_VERSION="d6f2bc8c046b4ec1b0d0c9132a49af087f4574a2"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2+"
 PKG_SITE="https://github.com/citra-emu/citra"
@@ -12,14 +12,14 @@ PKG_LONGDESC="A Nintendo 3DS Emulator"
 GET_HANDLER_SUPPORT="git"
 
 pre_configure_target() {
-  PKG_CMAKE_OPTS_TARGET="-DENABLE_SDL2=1 \
-                         -DENABLE_QT=1 \
-                         -DENABLE_CUBEB=0 \
-                         -DENABLE_FFMPEG=1 \
-                         -DCMAKE_BUILD_TYPE=\"Release\" \
-                         -DENABLE_WEB_SERVICE=0 \
-                         -DCMAKE_NO_SYSTEM_FROM_IMPORTED=1 \
-                         -DCMAKE_VERBOSE_MAKEFILE=1"
+  PKG_CMAKE_OPTS_TARGET="-D ENABLE_SDL2=1 \
+                         -D ENABLE_QT=1 \
+                         -D ENABLE_CUBEB=0 \
+                         -D ENABLE_FFMPEG=1 \
+                         -D CMAKE_BUILD_TYPE=\"Release\" \
+                         -D ENABLE_WEB_SERVICE=0 \
+                         -D CMAKE_NO_SYSTEM_FROM_IMPORTED=1 \
+                         -D CMAKE_VERBOSE_MAKEFILE=1"
 }
 
 pre_make_target() {
