8e86d4de35f463cdfab522fa0fcafc74d12b2e0d
5schatten <supervisedthinking@gmail.com>
2019-09-14 12:07:42 +0200

    emulationstation: added device specific config for AMLG12 / clean up

--- packages/5schatten/emulation/frontends/Emulationstation/emulationstation/package.mk
+++ packages/5schatten/emulation/frontends/Emulationstation/emulationstation/package.mk
@@ -7,7 +7,7 @@ PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/RetroPie/EmulationStation"
 PKG_URL="https://github.com/RetroPie/EmulationStation.git"
 PKG_DEPENDS_TARGET="toolchain linux glibc systemd dbus openssl zlib libpng alsa-lib SDL2-system freetype curl freeimage bzip2 vlc emulationstation-theme-carbon emulationstation-theme-simple-dark "
-PKG_LONGDESC="A Fork of Emulation Station for RetroPie. Emulation Station is a flexible emulator front-end supporting keyboardless navigation and custom system themes."
+PKG_LONGDESC="A Fork of Emulation Station which is a flexible emulator front-end supporting keyboardless navigation and custom system themes."
 GET_HANDLER_SUPPORT="git"
 PKG_GIT_CLONE_BRANCH="master"
 PKG_BUILD_FLAGS="+lto"
@@ -31,25 +31,32 @@ configure_package() {
 
 post_makeinstall_target() {
   # Create directories
-  mkdir -p $INSTALL/etc/emulationstation
-  mkdir -p $INSTALL/usr/config/emulationstation
-  mkdir -p $INSTALL/usr/lib/tmpfiles.d
+  mkdir -p ${INSTALL}/etc/emulationstation
+  mkdir -p ${INSTALL}/usr/config/emulationstation
+  mkdir -p ${INSTALL}/usr/lib/tmpfiles.d
 
   # Create symlinks for themes & config files
-  ln -s /usr/config/emulationstation/es_systems.cfg $INSTALL/etc/emulationstation/
-  ln -s /usr/config/emulationstation/themes         $INSTALL/etc/emulationstation/themes
+  ln -s /usr/config/emulationstation/es_systems.cfg ${INSTALL}/etc/emulationstation/
+  ln -s /usr/config/emulationstation/themes         ${INSTALL}/etc/emulationstation/themes
 
   # Install scripts
-  cp -rf $PKG_DIR/scripts/common/*     $INSTALL/usr/bin/
-  cp -rf $PKG_DIR/scripts/${PROJECT}/* $INSTALL/usr/bin/
+  cp -rf ${PKG_DIR}/scripts/common/*     ${INSTALL}/usr/bin/
+  cp -rf ${PKG_DIR}/scripts/${PROJECT}/* ${INSTALL}/usr/bin/
 
   # Install resources
-  cp -r $PKG_DIR/files/*     $INSTALL/usr/config/emulationstation/
-  cp -a $PKG_BUILD/resources $INSTALL/usr/config/emulationstation/
+  cp -r ${PKG_DIR}/files/*     ${INSTALL}/usr/config/emulationstation/
+  cp -a ${PKG_BUILD}/resources ${INSTALL}/usr/config/emulationstation/
 
   # Install config files
-  cp $PKG_DIR/config/es_input.cfg          $INSTALL/usr/config/emulationstation/
-  cp $PKG_DIR/config/es_settings.cfg       $INSTALL/usr/config/emulationstation/
-  cp $PKG_DIR/config/${PROJECT}/es_systems.cfg                 $INSTALL/usr/config/emulationstation/
-  cp $PKG_DIR/config/${PROJECT}/userdirs-emulationstation.conf $INSTALL/usr/lib/tmpfiles.d/
+  cp ${PKG_DIR}/config/es_input.cfg          ${INSTALL}/usr/config/emulationstation/
+  cp ${PKG_DIR}/config/es_settings.cfg       ${INSTALL}/usr/config/emulationstation/
+
+  if [ -d ${PKG_DIR}/config/device/${DEVICE} -a ! -z ${DEVICE} ]; then
+    cp -v ${PKG_DIR}/config/device/${DEVICE}/es_systems.cfg                 ${INSTALL}/usr/config/emulationstation/
+    cp -v ${PKG_DIR}/config/device/${DEVICE}/userdirs-emulationstation.conf ${INSTALL}/usr/lib/tmpfiles.d/
+  else
+    cp -v ${PKG_DIR}/config/${PROJECT}/es_systems.cfg                       ${INSTALL}/usr/config/emulationstation/
+    cp -v ${PKG_DIR}/config/${PROJECT}/userdirs-emulationstation.conf       ${INSTALL}/usr/lib/tmpfiles.d/
+  fi
 }
+
