d83bfd2e8b5d34f32b6e1f77cfa6c0166ad64ab7
5schatten <supervisedthinking@gmail.com>
2019-09-19 11:37:58 +0200

    tyrquake: updated to git d80db75

--- packages/5schatten/emulation/libretro/tyrquake/package.mk
+++ packages/5schatten/emulation/libretro/tyrquake/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="tyrquake"
-PKG_VERSION="28cd29b08be6515446558367f6ca5387a4a33c80"
-PKG_SHA256="0b713a2a138a1dcb36966905a269f1f045bd09ad4dbcbc4a45b2fc77a8df83f4"
+PKG_VERSION="d80db75a62dd444bf989c69552dc84ee359cd9a3"
+PKG_SHA256="13731ad166b6d423f10d60c5f830290977804e347ee4388dd9363cd510256948"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/tyrquake"
 PKG_URL="https://github.com/libretro/tyrquake/archive/${PKG_VERSION}.tar.gz"
