8af2eeb5753f0aa511000e6e511c6884a8944120
5schatten <supervisedthinking@gmail.com>
2019-09-20 13:54:35 +0200

    retroarch-joypad-autoconfig: updated to git 0fd6ff6

--- packages/5schatten/emulation/frontends/Retroarch/retroarch-depends/retroarch-joypad-autoconfig/package.mk
+++ packages/5schatten/emulation/frontends/Retroarch/retroarch-depends/retroarch-joypad-autoconfig/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="93227d68b420a1cb194db04a53d9f30dfface0c3"
-PKG_SHA256="6cdcd04b155bef8097048724c11beaf8957260d8f78b4a8bbc1b9e142cc0e807"
+PKG_VERSION="0fd6ff6296545db23600962f363303b1dde8e889"
+PKG_SHA256="ba1541022cb5f6705762ce31ceb7c371982592b0cbcdba0a68bd0abe0742e478"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/retroarch-joypad-autoconfig"
 PKG_URL="https://github.com/libretro/retroarch-joypad-autoconfig/archive/${PKG_VERSION}.tar.gz"
