e68ab8c3ba49bea6e3e44a3d8a9e4b362bb6b124
5schatten <supervisedthinking@gmail.com>
2019-09-24 22:59:25 +0200

    stella2014: updated to git 6d74ad9

--- packages/5schatten/emulation/libretro/stella2014/package.mk
+++ packages/5schatten/emulation/libretro/stella2014/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="stella2014"
-PKG_VERSION="a181878b283fc02c26c0474c41bde418c052c853"
-PKG_SHA256="5e52adcaf978d78cd0e182220882069011240206af1a5ad9bd3a06a9005d603a"
+PKG_VERSION="6d74ad9a0fd779145108cf1213229798d409ed37"
+PKG_SHA256="5995a0a882c2a7be82ce75dc63aa8b0deedfeb28363b29023466e9d8682b2ab3"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/stella2014-libretro"
 PKG_URL="https://github.com/libretro/stella2014-libretro/archive/${PKG_VERSION}.tar.gz"
