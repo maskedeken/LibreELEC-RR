9c22c27ef9a55567bb77d43184b655d934d4fcde
5schatten <supervisedthinking@gmail.com>
2019-09-26 12:45:29 +0200

    libglvnd: updated to v1.2.0 / dropped upstreamed pkgconfig patch

--- packages/graphics/libglvnd/package.mk
+++ packages/graphics/libglvnd/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libglvnd"
-PKG_VERSION="1.1.1"
-PKG_SHA256="baca5e1a78b96a112650cdc597be3f856d4754eb73a7bf3f6629e78a7e9f2b5a"
+PKG_VERSION="1.2.0"
+PKG_SHA256="37981bd3320261f14140b8aef6e2e6c08c7e75b6a98dcea034670017f3f1312a"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/NVIDIA/libglvnd"
 PKG_URL="https://github.com/NVIDIA/libglvnd/archive/v${PKG_VERSION}.tar.gz"
