59c428a9087e168ac2b51b5a964ca7c7835116d8
5schatten <supervisedthinking@gmail.com>
2019-10-05 11:31:11 +0200

    opus-system: updated to v1.3.1 / switched to CMake toolchain

--- packages/5schatten/audio/opus-system/package.mk
+++ packages/5schatten/audio/opus-system/package.mk
@@ -2,20 +2,20 @@
 # Copyright (C) 2019-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="opus-system"
-PKG_VERSION="1.3"
-PKG_SHA256="4f3d69aefdf2dbaf9825408e452a8a414ffc60494c70633560700398820dc550"
+PKG_VERSION="1.3.1"
+PKG_SHA256="4834a8944c33a7ecab5cad9454eeabe4680ca1842cb8f5a2437572dbf636de8f"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.opus-codec.org"
-PKG_URL="https://archive.mozilla.org/pub/opus/opus-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/xiph/opus/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Codec designed for interactive speech and audio transmission over the Internet."
 
 pre_configure_target() {
-  PKG_CONFIGURE_OPTS_TARGET=""
+  PKG_CMAKE_OPTS_TARGET="-D BUILD_SHARED_LIBS=ON"
 
   if [ "${TARGET_ARCH}" = "arm" ]; then
-    PKG_CONFIGURE_OPTS_TARGET+=" --enable-fixed-point"
-  else
-    PKG_CONFIGURE_OPTS_TARGET+=" --disable-fixed-point"
+    if ! target_has_feature neon; then
+      PKG_CMAKE_OPTS_TARGET+=" -D OPUS_FIXED_POINT=ON"
+    fi
   fi
 }
