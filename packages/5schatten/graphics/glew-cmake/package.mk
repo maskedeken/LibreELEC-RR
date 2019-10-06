b5a99014bda1653dc4a15ca5a8c95c10c774a3b9
5schatten <supervisedthinking@gmail.com>
2019-10-06 17:31:01 +0200

    glew-cmake: build with GLU / added dependency glu

--- packages/5schatten/graphics/glew-cmake/package.mk
+++ packages/5schatten/graphics/glew-cmake/package.mk
@@ -7,9 +7,10 @@ PKG_SHA256="756d207712263f33f9a719998bd60dc829e17f48431a53fec6f75a58d6e4f931"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/Perlmint/glew-cmake"
 PKG_URL="https://github.com/Perlmint/glew-cmake/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libXi"
+PKG_DEPENDS_TARGET="toolchain libXi glu"
 PKG_LONGDESC="The OpenGL Extension Wrangler Library (GLEW) is a cross-platform open-source C/C++ extension loading library."
 
 PKG_CMAKE_OPTS_TARGET="-D glew-cmake_BUILD_STATIC=off \
                        -D glew-cmake_BUILD_MULTI_CONTEXT=off \
+                       -D USE_GLU=ON \
                        -D PKG_CONFIG_REPRESENTATIVE_TARGET="libglew_shared""
