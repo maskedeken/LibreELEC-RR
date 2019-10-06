be9289e032b148c4c330a89b51aaa52f1bdfa610
5schatten <supervisedthinking@gmail.com>
2019-10-06 17:11:54 +0200

    glew-cmake: updated to git 36ec731 / 2.2.0 RC2

--- packages/5schatten/graphics/glew-cmake/package.mk
+++ packages/5schatten/graphics/glew-cmake/package.mk
@@ -2,13 +2,14 @@
 # Copyright (C) 2019-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="glew-cmake"
-PKG_VERSION="2.1.0"
-PKG_SHA256="e1dd68b24dafa1d7c26fb59a38a3daa7ca749962104c486eaa0751f967d7f8c4"
+PKG_VERSION="36ec731e27e040bd7b5d4a6a4e1bffea75520128" #2.2.0 RC2
+PKG_SHA256="756d207712263f33f9a719998bd60dc829e17f48431a53fec6f75a58d6e4f931"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/Perlmint/glew-cmake"
-PKG_URL="https://github.com/Perlmint/glew-cmake/archive/glew-cmake-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/Perlmint/glew-cmake/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libXi"
 PKG_LONGDESC="The OpenGL Extension Wrangler Library (GLEW) is a cross-platform open-source C/C++ extension loading library."
 
 PKG_CMAKE_OPTS_TARGET="-D glew-cmake_BUILD_STATIC=off \
-                       -D glew-cmake_BUILD_MULTI_CONTEXT=off"
+                       -D glew-cmake_BUILD_MULTI_CONTEXT=off \
+                       -D PKG_CONFIG_REPRESENTATIVE_TARGET="libglew_shared""
