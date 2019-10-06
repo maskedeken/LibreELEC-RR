7b3c575a9ca2d9964a8e19ae65ae6f116903d9f3
5schatten <supervisedthinking@gmail.com>
2019-10-06 21:37:03 +0200

    gstreamer: updated to v1.16.1

--- packages/5schatten/multimedia/GStreamer/gstreamer/package.mk
+++ packages/5schatten/multimedia/GStreamer/gstreamer/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="gstreamer"
-PKG_VERSION="1.16.0"
-PKG_SHA256="0e8e2f7118be437cba879353970cf83c2acced825ecb9275ba05d9186ef07c00"
+PKG_VERSION="1.16.1"
+PKG_SHA256="02211c3447c4daa55919c5c0f43a82a6fbb51740d57fc3af0639d46f1cf4377d"
 PKG_LICENSE="GPL"
 PKG_SITE="https://gstreamer.freedesktop.org"
 PKG_URL="https://gstreamer.freedesktop.org/src/gstreamer/${PKG_NAME}-${PKG_VERSION}.tar.xz"
