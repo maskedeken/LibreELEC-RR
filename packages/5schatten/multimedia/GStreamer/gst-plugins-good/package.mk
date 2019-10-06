260b6b953d05b7f41c91f29b373fae66127a10e9
5schatten <supervisedthinking@gmail.com>
2019-10-06 21:37:26 +0200

    gst-plugins-good: updated to v1.16.1

--- packages/5schatten/multimedia/GStreamer/gst-plugins-good/package.mk
+++ packages/5schatten/multimedia/GStreamer/gst-plugins-good/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="gst-plugins-good"
-PKG_VERSION="1.16.0"
-PKG_SHA256="654adef33380d604112f702c2927574cfc285e31307b79e584113858838bb0fd"
+PKG_VERSION="1.16.1"
+PKG_SHA256="9fbabe69018fcec707df0b71150168776040cde6c1a26bb5a82a136755fa8f1f"
 PKG_LICENSE="GPL"
 PKG_SITE="https://gstreamer.freedesktop.org/modules/gst-plugins-good.html"
 PKG_URL="https://gstreamer.freedesktop.org/src/gst-plugins-good/${PKG_NAME}-${PKG_VERSION}.tar.xz"
