222bdfb87bc352e3249133b378200a5b84f230a6
5schatten <supervisedthinking@gmail.com>
2019-10-07 10:19:22 +0200

    dolphin: updated to r5.0-10960

--- packages/5schatten/emulation/standalone/dolphin/package.mk
+++ packages/5schatten/emulation/standalone/dolphin/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="dolphin"
-PKG_VERSION="b6545ea285b25765bbf464774f9a8e7649f6bc15" #5.0-10956
+PKG_VERSION="19ed64122c11f7cd36ff92b61e7af7bb772c01a8" #5.0-10960
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/dolphin-emu/dolphin"
