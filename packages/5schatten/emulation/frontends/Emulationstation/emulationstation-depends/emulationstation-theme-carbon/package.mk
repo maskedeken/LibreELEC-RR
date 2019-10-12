734b4a4c7ff467d154eb9050612127dd11f00313
5schatten <supervisedthinking@gmail.com>
2019-10-12 21:00:55 +0200

    emulationstation-theme-carbon: added openbor theme

--- packages/5schatten/emulation/frontends/Emulationstation/emulationstation-depends/emulationstation-theme-carbon/package.mk
+++ packages/5schatten/emulation/frontends/Emulationstation/emulationstation-depends/emulationstation-theme-carbon/package.mk
@@ -29,7 +29,8 @@ makeinstall_target() {
 
   # Add missing generic systems
   if [ "$PROJECT" = "Generic" ]; then
-    cp -a $PKG_DIR/files/n3ds $INSTALL/${ES_THEME_PATH}/
-    cp -a $PKG_DIR/files/ps3  $INSTALL/${ES_THEME_PATH}/
+    cp -a $PKG_DIR/files/n3ds    $INSTALL/${ES_THEME_PATH}/
+    cp -a $PKG_DIR/files/openbor $INSTALL/${ES_THEME_PATH}/
+    cp -a $PKG_DIR/files/ps3     $INSTALL/${ES_THEME_PATH}/
   fi
 }
